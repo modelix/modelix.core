3f562b104fb464cd2f6cea0b0087e56ff75d1697
Sascha Lisson <sl@slisson.de>
2022-09-30 17:19:57 +0200

    model server moved to here
    From https://github.com/modelix/modelix/commit/b783deb42d2a01718efb85552a413b8a39d6c317

---
+++ modelix-version.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+cd "$(dirname "$0")"
+
+FILE=./version.txt
+if [ -f "$FILE" ]; then
+    MODELIX_VERSION="`cat $FILE`"
+else
+    MODELIX_VERSION="$(date +"%Y%m%d%H%M")-SNAPSHOT"
+    echo "$MODELIX_VERSION" > $FILE
+fi
+
+echo "$MODELIX_VERSION"

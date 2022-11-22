8978f6764707effe6da99067589ab4f06660123f
Sascha Lisson <sl@slisson.de>
2022-11-22 19:16:35 +0100

    extension function IBranch.getArea
    branch.getArea() is more intuitive than PArea(branch)

--- model-api/src/commonMain/kotlin/org/modelix/model/area/PArea.kt
+++ model-api/src/commonMain/kotlin/org/modelix/model/area/PArea.kt
@@ -11,19 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.modelix.model.area
 
 import org.modelix.model.api.*
@@ -94,3 +81,5 @@ class PArea(val branch: IBranch) : IArea {
 
     data class AreaReference(val branchId: String?) : IAreaReference
 }
+
+fun IBranch.getArea() = PArea(this)

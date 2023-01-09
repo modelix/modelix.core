f07cb4a0e90bfe779f16d6dc6583833a7ef8a2ec
Sascha Lisson <sl@slisson.de>
2023-01-09 16:37:03 +0100

    ...

---
+++ editor-runtime/src/commonMain/kotlin/org/modelix/editor/INonExistingNode.kt
@@ -0,0 +1,106 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.modelix.editor
+
+import org.modelix.model.api.IChildLink
+import org.modelix.model.api.IConcept
+import org.modelix.model.api.INode
+import org.modelix.model.api.addNewChild
+import org.modelix.model.api.getChildren
+import org.modelix.model.api.getContainmentLink
+import org.modelix.model.api.index
+import org.modelix.model.api.remove
+
+interface INonExistingNode {
+    fun getParent(): INonExistingNode?
+    fun replaceNode(subConcept: IConcept?): INode
+    fun getOrCreateNode(subConcept: IConcept?): INode
+    fun expectedConcept(): IConcept?
+}
+
+data class SpecializedNonExistingNode(val node: INonExistingNode, val subConcept: IConcept): INonExistingNode {
+    override fun getParent(): INonExistingNode? {
+        TODO("Not yet implemented")
+    }
+
+    override fun replaceNode(subConcept: IConcept?): INode {
+        return node.replaceNode(coerceOutputConcept(subConcept))
+    }
+
+    override fun getOrCreateNode(subConcept: IConcept?): INode {
+        return node.getOrCreateNode(coerceOutputConcept(subConcept))
+    }
+
+    override fun expectedConcept(): IConcept {
+        return subConcept
+    }
+}
+
+fun INonExistingNode.coerceOutputConcept(subConcept: IConcept?): IConcept? {
+    val expectedConcept = expectedConcept()
+    return if (subConcept != null) {
+        require(subConcept.isSubConceptOf(expectedConcept)) {
+            "$subConcept is not a sub-concept of $expectedConcept"
+        }
+        subConcept
+    } else {
+        expectedConcept
+    }
+}
+
+data class ExistingNode(val node: INode) : INonExistingNode {
+    override fun getParent(): INonExistingNode? = node.parent?.let { ExistingNode(it) }
+
+    override fun replaceNode(subConcept: IConcept?): INode {
+        val parent = node.parent ?: throw RuntimeException("cannot replace the root node")
+        val newNode = parent.addNewChild(node.roleInParent, node.index(), coerceOutputConcept(subConcept))
+        node.remove()
+        return newNode
+    }
+
+    override fun getOrCreateNode(subConcept: IConcept?): INode {
+        val outputConcept = coerceOutputConcept(subConcept)
+        return if (node.concept?.isSubConceptOf(outputConcept) == true) {
+            node
+        } else {
+            replaceNode(subConcept)
+        }
+    }
+
+    override fun expectedConcept(): IConcept? {
+        return node.getContainmentLink()?.targetConcept
+    }
+}
+
+fun INode.toNonExisting() = ExistingNode(this)
+
+data class NonExistingChild(private val parent: INonExistingNode, val link: IChildLink, val index: Int = 0) : INonExistingNode {
+    override fun getParent() = parent
+
+    override fun replaceNode(subConcept: IConcept?): INode {
+        val parentNode = parent.getOrCreateNode(null)
+        val existing = parentNode.getChildren(link).toList().getOrNull(index)
+        val newNode = parentNode.addNewChild(link, index, coerceOutputConcept(subConcept))
+        existing?.remove()
+        return newNode
+    }
+
+    override fun getOrCreateNode(subConcept: IConcept?): INode {
+        return replaceNode(subConcept)
+    }
+
+    override fun expectedConcept(): IConcept {
+        return link.targetConcept
+    }
+}

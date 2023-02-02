e330fb5597a24d1bb07ed00ee102890405f28e50
Michael Huster <mhuster@itemis.com>
2023-02-02 16:44:33 +0100

    extended name config by suffixes and updated default values

--- metamodel-generator/src/main/kotlin/org/modelix/metamodel/generator/NameConfig.kt
+++ metamodel-generator/src/main/kotlin/org/modelix/metamodel/generator/NameConfig.kt
@@ -1,22 +1,39 @@
 package org.modelix.metamodel.generator
 
 class NameConfig(
-    private val languagePrefix: String = "L_",
-    private val nodeWrapperInterfacePrefix: String = "N_",
-    private val nodeWrapperImplPrefix: String = "_N_TypedImpl_",
-    private val conceptObjectPrefix: String = "_C_UntypedImpl_",
-    private val conceptWrapperInterfacePrefix: String = "C_",
-    private val conceptWrapperImplPrefix: String = "_C_TypedImpl_"
+    private val languagePrefix: String = "",
+    private val languageSuffix: String = "Lang",
+    private val nodeWrapperInterfacePrefix: String = "",
+    private val nodeWrapperInterfaceSuffix: String = "Node",
+    private val nodeWrapperImplPrefix: String = "",
+    private val nodeWrapperImplSuffix: String = "NodeTypedImpl",
+    private val conceptObjectPrefix: String = "",
+    private val conceptObjectSuffix: String = "ConceptUntypedImpl",
+    private val conceptWrapperInterfacePrefix: String = "",
+    private val conceptWrapperInterfaceSuffix: String = "Concept",
+    private val conceptWrapperImplPrefix: String = "",
+    private val conceptWrapperImplSuffix: String = "ConceptTypedImpl"
 ) {
 
-    fun languageClassName(baseName: String) = languagePrefix + baseName.replace(".", "_")
-    fun nodeWrapperInterfaceName(baseName: String) = baseName.fqNamePrefix(nodeWrapperInterfacePrefix)
-    fun nodeWrapperImplName(baseName: String) = baseName.fqNamePrefix(nodeWrapperImplPrefix)
-    fun conceptObjectName(baseName: String) = baseName.fqNamePrefix(conceptObjectPrefix)
-    fun conceptWrapperInterfaceName(baseName: String) = baseName.fqNamePrefix(conceptWrapperInterfacePrefix)
-    fun conceptWrapperImplName(baseName: String) = baseName.fqNamePrefix(conceptWrapperImplPrefix)
+    fun languageClassName(baseName: String) =
+        languagePrefix + baseName.replace(".", "_") + languageSuffix
 
-    private fun String.fqNamePrefix(prefix: String, suffix: String = ""): String {
+    fun nodeWrapperInterfaceName(baseName: String) =
+        baseName.fqNamePrefix(nodeWrapperInterfacePrefix, nodeWrapperInterfaceSuffix)
+
+    fun nodeWrapperImplName(baseName: String) =
+        baseName.fqNamePrefix(nodeWrapperImplPrefix, nodeWrapperImplSuffix)
+
+    fun conceptObjectName(baseName: String) =
+        baseName.fqNamePrefix(conceptObjectPrefix, conceptObjectSuffix)
+
+    fun conceptWrapperInterfaceName(baseName: String) =
+        baseName.fqNamePrefix(conceptWrapperInterfacePrefix, conceptWrapperInterfaceSuffix)
+
+    fun conceptWrapperImplName(baseName: String) =
+        baseName.fqNamePrefix(conceptWrapperImplPrefix, conceptWrapperImplSuffix)
+
+    private fun String.fqNamePrefix(prefix: String, suffix: String): String {
         return if (this.contains(".")) {
             this.substringBeforeLast(".") + "." + prefix + this.substringAfterLast(".")
         } else {

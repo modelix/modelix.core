b1d0d27a8e905e4709ef33068d7e7f36f839dd12
Johannes Wienke <jwienke@itemis.com>
2023-02-13 17:31:05 +0100

    ci: enforce known commit scopes
    This ensures that we have a predictable changelog with known headings.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "light-model-client",
+        "light-model-server",
+        "metamodel-export-mps",
+        "metamodel-generator",
+        "metamodel-gradle",
+        "model-api",
+        "model-client",
+        "model-server",
+        "ts-model-api",
+      ],
+    ],
+  },
+};

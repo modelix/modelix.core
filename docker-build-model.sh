7f54c2369730e1ae892aa3a0f2a4f284292dbfe7
Michael Huster <mhuster@itemis.com>
2023-02-14 15:40:27 +0100

    integrated tagging and pushing into build

--- docker-build-model.sh
+++ docker-build-model.sh
@@ -6,10 +6,11 @@ TAG=$( ./modelix-version.sh )
 
 (
   cd model-server
-  docker buildx build --platform linux/amd64,linux/arm64 --no-cache -t modelix/modelix-model .
+  docker buildx build --platform linux/amd64,linux/arm64 --push --no-cache -t modelix/modelix-model:latest \
+  -t "modelix/modelix-model:${TAG}" .
 )
 
-docker tag modelix/modelix-model:latest "modelix/modelix-model:${TAG}"
+# docker tag modelix/modelix-model:latest "modelix/modelix-model:${TAG}"
 
 if [ -f "../modelix/helm" ]; then
   sed -i.bak -E "s/  model: \".*\"/  model: \"${TAG}\"/" ../modelix/helm/dev.yaml
--- docker-ci.sh
+++ docker-ci.sh
@@ -6,16 +6,16 @@ docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_KEY"
 
 ./docker-build-model.sh
 
-MODELIX_VERSION=$( ./modelix-version.sh )
-TAGS="$MODELIX_VERSION" # list separated by space
-IMAGE_NAMES="model" # list separated by space
-for TAG in $TAGS ; do
-  echo "Pushing Tag $TAG"
-
-  for IMAGE_NAME in $IMAGE_NAMES ; do
-    if [ "$TAG" != "latest" ]; then
-      docker tag "modelix/modelix-$IMAGE_NAME:latest" "modelix/modelix-$IMAGE_NAME:${TAG}"
-    fi
-    docker push "modelix/modelix-$IMAGE_NAME:${TAG}"
-  done
-done
+#MODELIX_VERSION=$( ./modelix-version.sh )
+#TAGS="$MODELIX_VERSION" # list separated by space
+#IMAGE_NAMES="model" # list separated by space
+#for TAG in $TAGS ; do
+#  echo "Pushing Tag $TAG"
+#
+#  for IMAGE_NAME in $IMAGE_NAMES ; do
+#    if [ "$TAG" != "latest" ]; then
+#      docker tag "modelix/modelix-$IMAGE_NAME:latest" "modelix/modelix-$IMAGE_NAME:${TAG}"
+#    fi
+#    docker push "modelix/modelix-$IMAGE_NAME:${TAG}"
+#  done
+#done

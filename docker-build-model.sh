6040771224f9bca5b0b552b05564b9300a42cfb3
Michael Huster <mhuster@itemis.com>
2023-02-15 16:24:26 +0100

    introduced ci flag to reenable local builds

--- docker-build-model.sh
+++ docker-build-model.sh
@@ -3,11 +3,17 @@
 set -e
 
 TAG=$( ./modelix-version.sh )
+MODELIX_TARGET_PLATFORM="${MODELIX_TARGET_PLATFORM:=linux/amd64}"
 
 (
   cd model-server
-  docker buildx build --platform linux/amd64,linux/arm64 --push --no-cache -t modelix/modelix-model:latest \
-  -t "modelix/modelix-model:${TAG}" .
+  if [[ -z "${MODELIX_CI}" ]]; then
+    docker build --platform ${MODELIX_TARGET_PLATFORM} --no-cache \
+    -t modelix/modelix-model:latest -t "modelix/modelix-model:${TAG}" .
+  else
+    docker buildx build --platform linux/amd64,linux/arm64 --push --no-cache \
+    -t modelix/modelix-model:latest -t "modelix/modelix-model:${TAG}" .
+  fi
 )
 
 if [ -f "../modelix/helm" ]; then

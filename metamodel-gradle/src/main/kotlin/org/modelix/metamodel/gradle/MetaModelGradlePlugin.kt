79ee729923c4f1afefeb9d2b034241d1cd262a6d
Michael Huster <mhuster@itemis.com>
2023-03-07 11:47:08 +0100

    added jsonDir to override exported-languages

--- metamodel-gradle/src/main/kotlin/org/modelix/metamodel/gradle/MetaModelGradlePlugin.kt
+++ metamodel-gradle/src/main/kotlin/org/modelix/metamodel/gradle/MetaModelGradlePlugin.kt
@@ -23,11 +23,13 @@ class MetaModelGradlePlugin: Plugin<Project> {
         val modelixCoreVersion = readModelixCoreVersion() ?: throw RuntimeException("modelix.core version not found")
         project.dependencies.add(exporterDependencies.name, "org.modelix:metamodel-export-mps:$modelixCoreVersion")
         val downloadExporterDependencies = project.tasks.register("downloadMetaModelExporter", Sync::class.java) { task ->
+            task.enabled = settings.jsonDir == null
             task.from(exporterDependencies.resolve().map { project.zipTree(it) })
             task.into(exporterDir)
         }
 
         val generateAntScriptForMpsMetaModelExport = project.tasks.register("generateAntScriptForMpsMetaModelExport", GenerateAntScriptForMpsMetaModelExport::class.java) { task ->
+            task.enabled = settings.jsonDir == null
             task.dependsOn(downloadExporterDependencies)
             task.dependsOn(*settings.taskDependencies.toTypedArray())
             task.mpsHome.set(getMpsHome())
@@ -43,6 +45,7 @@ class MetaModelGradlePlugin: Plugin<Project> {
 
         val exportedLanguagesDir = getBuildOutputDir().resolve("exported-languages")
         val exportMetaModelFromMps = project.tasks.register("exportMetaModelFromMps", JavaExec::class.java) { task ->
+            task.enabled = settings.jsonDir == null
             task.workingDir = getBuildOutputDir()
             task.mainClass.set("org.apache.tools.ant.launch.Launcher")
             task.classpath(antDependencies)
@@ -83,9 +86,14 @@ class MetaModelGradlePlugin: Plugin<Project> {
                 task.includedNamespaces.addAll(settings.includedLanguageNamespaces)
                 task.includedLanguages.addAll(settings.includedLanguages)
                 task.includedConcepts.addAll(settings.includedConcepts)
-                task.exportedLanguagesDir.set(exportedLanguagesDir)
+                if (settings.jsonDir != null) {
+                    task.exportedLanguagesDir.set(settings.jsonDir)
+                } else {
+                    task.exportedLanguagesDir.set(exportedLanguagesDir)
+                }
                 settings.registrationHelperName?.let { task.registrationHelperName.set(it) }
                 task.nameConfig.set(settings.nameConfig)
+
             }
         }
 

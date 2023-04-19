7143a9699f2cefea4bd897b45490bd4d7dece6b5
Sascha Lisson <sl@slisson.de>
2023-04-19 18:01:57 +0200

    fix(ts-model-api): import of @modelix/ts-model-api was missing
    In the JS code generated by the Kotlin compiler the import was missing.

--- ts-model-api/build.gradle.kts
+++ ts-model-api/build.gradle.kts
@@ -1,14 +1,13 @@
 import com.github.gradle.node.npm.task.NpmTask
-import com.github.gradle.node.task.NodeTask
 
 plugins {
   base
-  id("com.github.node-gradle.node") version "3.4.0"
+  id("com.github.node-gradle.node") version "3.5.1"
 }
 
 node {
-    version.set("18.3.0")
-    npmVersion.set("8.11.0")
+    version.set("18.12.1")
+    npmVersion.set("8.19.2")
     download.set(true)
 }
 
@@ -20,9 +19,38 @@ tasks.named("npm_run_build") {
   outputs.dir("dist")
 }
 
+val patchKotlinExternals = tasks.create("patchKotlinExternals") {
+  dependsOn("npm_run_generateKotlin")
+  doLast {
+    val annotationLine = """@file:JsModule("@modelix/ts-model-api") @file:JsNonModule"""
+    val dukatDir = buildDir.resolve("dukat")
+    val files = dukatDir.listFiles()?.toList() ?: emptyList()
+    val matchingFiles = files.filter { it.name.contains("@modelix_ts-model-api") }
+    if (matchingFiles.isEmpty()) throw RuntimeException("No files found for patching in $dukatDir")
+    val typealiases = HashSet<String>()
+    val allImports = HashSet<String>()
+    for (file in matchingFiles) {
+      var lines = file.readLines()
+      if (lines.isEmpty()) continue
+      if (lines.first() == annotationLine) continue
+      lines = listOf(annotationLine) + lines
+      typealiases += lines.filter { it.startsWith("typealias ") }
+      allImports += lines.filter { it.startsWith("import ") }
+      lines = lines.filterNot { it.startsWith("typealias ") }
+      file.writeText(lines.joinToString("\n"))
+    }
+    dukatDir.resolve("typealiases.kt").writeText(allImports.joinToString("\n") + "\n\n" + typealiases.joinToString("\n"))
+  }
+}
+
 tasks.named("assemble") {
   dependsOn("npm_run_build")
   dependsOn("npm_run_generateKotlin")
+  dependsOn(patchKotlinExternals)
+}
+
+tasks.named("npm_run_generateKotlin") {
+  finalizedBy(patchKotlinExternals)
 }
 
 val updateVersion = tasks.register<NpmTask>("updateVersion") {

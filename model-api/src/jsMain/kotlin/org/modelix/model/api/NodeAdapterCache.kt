381f4ad76906f53198e672b5fe530efe4d15a018
Sascha Lisson <sl@slisson.de>
2023-04-19 18:01:57 +0200

    fix(ts-model-api): import of @modelix/ts-model-api was missing
    In the JS code generated by the Kotlin compiler the import was missing.

--- model-api/src/jsMain/kotlin/org/modelix/model/api/NodeAdapterCache.kt
+++ model-api/src/jsMain/kotlin/org/modelix/model/api/NodeAdapterCache.kt
@@ -1,5 +1,6 @@
 package org.modelix.model.api
 
+import TypedNode
 import kotlinx.browser.window
 
 @JsExport
@@ -43,13 +44,7 @@ object NodeAdapterCache {
     private fun toINode(node: Any): INode {
         if (node is INode) return node
         if (node is NodeAdapterJS) return node.node
-
-        // if (node is TypedNode) return toINode(node.node)
-
-        // Workaround, because the line above fails with 'TypedNode is not defined'.
-        // The import for '@modelix/ts-model-api' seems to be missing in the generated JS.
-        val unwrapped = node.asDynamic().node
-        if (unwrapped != null) return toINode(unwrapped)
+        if (node is TypedNode) return toINode(node.node)
 
         throw IllegalArgumentException("Unsupported node type: $node")
     }

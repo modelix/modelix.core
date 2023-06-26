62c869ef05a5a2f7be1edc442a094b29c15a2c53
Sascha Lisson <sascha.lisson@itemis.de>
2023-06-26 14:16:06 +0200

    feat(modelql): .mapLocal2

--- modelql-core/src/commonMain/kotlin/org/modelix/modelql/core/LocalMappingStep.kt
+++ modelql-core/src/commonMain/kotlin/org/modelix/modelql/core/LocalMappingStep.kt
@@ -7,6 +7,7 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.modules.SerializersModule
+import kotlin.experimental.ExperimentalTypeInference
 
 open class LocalMappingStep<In, Out>(val transformation: (In) -> Out) : MonoTransformingStep<In, Out>() {
     override fun getOutputSerializer(serializersModule: SerializersModule): KSerializer<out Out> {
@@ -24,6 +25,39 @@ open class LocalMappingStep<In, Out>(val transformation: (In) -> Out) : MonoTran
     override fun toString(): String {
         return "${getProducer()}.mapLocal()"
     }
+}
+
+private class LocalMappingBuilder<In, Out> : ILocalMappingBuilder<In, Out> {
+    private val valueRequests = ArrayList<AsyncBuilder.ValueRequest<Any?>>()
+    private var resultHandler: (() -> Out)? = null
+
+    fun compileOutputStep(): IMonoStep<IZipOutput<*>> {
+        val allRequestSteps: List<IMonoStep<Any?>> = valueRequests.map { it.step }
+        return zipList(*allRequestSteps.toTypedArray())
+    }
+
+    fun processResult(result: IZipOutput<*>): Out {
+        val allRequests: List<AsyncBuilder.Request<Any?>> = valueRequests
+        allRequests.zip(result.values).forEach { (request, value) ->
+            request.set(value)
+        }
+
+        return resultHandler!!.invoke()
+    }
+
+    override fun onSuccess(body: () -> Out) {
+        resultHandler = body
+    }
+    override fun <T> IMonoStep<T>.getLater(): AsyncBuilder.ValueRequest<T> {
+        return AsyncBuilder.ValueRequest(this).also { valueRequests.add(it as AsyncBuilder.ValueRequest<Any?>) }
+    }
+}
+
+interface ILocalMappingBuilder<In, Out> {
+    @OptIn(ExperimentalTypeInference::class)
+    @BuilderInference
+    fun onSuccess(body: () -> Out)
+    fun <T> IMonoStep<T>.getLater(): IValueRequest<T>
 }
 
 class LocalMappingSerializer<In, Out>(val step: LocalMappingStep<In, Out>, val inputSerializer: KSerializer<out In>) : KSerializer<Out> {
@@ -47,6 +81,18 @@ class ExecuteLocalStep<In, Out>(transformation: (In) -> Out) : LocalMappingStep<
     override fun toString(): String {
         return "${getProducer()}.executeLocal()"
     }
+}
+
+fun <In, Out> IFluxStep<In>.mapLocal2(body: ILocalMappingBuilder<In, Out>.(IMonoStep<In>) -> Unit): IFluxStep<Out> {
+    return map { it.mapLocal2(body) }
+}
+
+fun <In, Out> IMonoStep<In>.mapLocal2(body: ILocalMappingBuilder<In, Out>.(IMonoStep<In>) -> Unit): IMonoStep<Out> {
+    val builder = LocalMappingBuilder<In, Out>()
+    builder.apply {
+        body(this@mapLocal2)
+    }
+    return builder.compileOutputStep().mapLocal { builder.processResult(it) }
 }
 
 fun <In, Out> IMonoStep<In>.mapLocal(body: (In) -> Out) = LocalMappingStep(body).connectAndDowncast(this)

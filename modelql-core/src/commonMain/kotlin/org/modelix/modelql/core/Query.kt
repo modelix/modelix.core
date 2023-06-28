d2e8e9b5a3f5d5f728161c26d6ab1af90b88edf1
Sascha Lisson <sascha.lisson@itemis.de>
2023-06-28 20:10:16 +0200

    fix(modelql): format

--- modelql-core/src/commonMain/kotlin/org/modelix/modelql/core/Query.kt
+++ modelql-core/src/commonMain/kotlin/org/modelix/modelql/core/Query.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.single

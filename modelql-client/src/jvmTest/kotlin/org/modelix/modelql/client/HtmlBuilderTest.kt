9bb4db8b28f7050178e37389cc172dbbc76d4c53
Sascha Lisson <sascha.lisson@itemis.de>
2023-07-03 15:22:41 +0200

    feat(modelql): WIP html templates

--- modelql-client/src/jvmTest/kotlin/org/modelix/modelql/client/HtmlBuilderTest.kt
+++ modelql-client/src/jvmTest/kotlin/org/modelix/modelql/client/HtmlBuilderTest.kt
@@ -22,11 +22,13 @@ import org.modelix.model.persistent.MapBaseStore
 import org.modelix.model.server.light.LightModelServer
 import org.modelix.modelql.core.AsyncBuilder
 import org.modelix.modelql.core.IAsyncBuilder
+import org.modelix.modelql.core.IFluxStep
+import org.modelix.modelql.core.IMonoStep
 import org.modelix.modelql.core.mapLocal
+import org.modelix.modelql.core.toList
 import org.modelix.modelql.untyped.buildQuery
 import org.modelix.modelql.untyped.children
 import org.modelix.modelql.untyped.property
-import org.modelix.modelql.untyped.query
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.time.Duration.Companion.seconds
@@ -58,9 +60,10 @@ class HtmlBuilderTest {
     fun modular() = runTest { httpClient ->
         val client = ModelQLClient("http://localhost/query", httpClient)
 
-        fun HtmlBuilder<INode>.renderModel() {
-            val name = input.property("name").getLater()
-            onSuccess {
+        val modelTemplate = buildModelQLTemplate<INode, FlowContent> {
+            val name = input.property("name").prepare()
+
+            templateBody {
                 div {
                     h2 {
                         +"Model: ${name.get()}"
@@ -69,33 +72,29 @@ class HtmlBuilderTest {
             }
         }
 
-        fun HtmlBuilder<INode>.renderModule() {
-            val name = input.property("name").getLater()
-            val models = input.children("models").iterateLater {
-                renderModel()
-            }
-            onSuccess {
+        val moduleTemplate = buildModelQLTemplate<INode, FlowContent> {
+            val name = input.property("name").prepare()
+            val models = input.children("models").prepareTemplate(modelTemplate)
+
+            templateBody {
                 div {
                     h1 {
                         +"Module: ${name.get()}"
                     }
-                    iterate(models)
+                    applyTemplates(models)
                 }
             }
         }
 
-        fun HtmlBuilder<INode>.renderRepository() {
-            val modules = input.children("modules").iterateLater {
-                renderModule()
-            }
-            onSuccess {
-                iterate(modules)
+        val repositoryTemplate = buildModelQLTemplate<INode, FlowContent> {
+            val modules = input.children("modules").prepareTemplate(moduleTemplate)
+
+            templateBody {
+                applyTemplates(modules)
             }
         }
 
-        val actual = client.getRootNode().queryAndBuildHtml {
-            renderRepository()
-        }
+        val actual = client.getRootNode().queryAndBuildHtml(repositoryTemplate)
         val expected = """<html><body><div><h1>Module: abc</h1><div><h2>Model: model1a</h2></div><div><h2>Model: model1b</h2></div></div></body></html>"""
         assertEquals(expected, actual)
     }
@@ -103,9 +102,9 @@ class HtmlBuilderTest {
 
 typealias HtmlBuilder<In> = IAsyncBuilder<In, FlowContent>
 
-suspend fun INode.queryAndBuildHtml(body: HtmlBuilder<INode>.() -> Unit): String {
+suspend fun INode.queryAndBuildHtml(template: IModelQLTemplate<INode, HTML>): String {
     val query = buildQuery<String> { repository ->
-        val htmlBuilder = AsyncBuilder<INode, FlowContent>(repository)
+        val htmlBuilder = AsyncBuilder<INode, HTML>(repository)
         htmlBuilder.apply(body)
         htmlBuilder.compileOutputStep().mapLocal { result ->
             createHTML(prettyPrint = false).html {
@@ -119,4 +118,90 @@ suspend fun INode.queryAndBuildHtml(body: HtmlBuilder<INode>.() -> Unit): String
     }
     println("query: $query")
     return query.execute()
+}
+
+fun <In, Context> buildModelQLTemplate(body: IModelQLTemplateBuilder<In, Context>.() -> Unit): IModelQLTemplate<In, Context> {
+    return object : IModelQLTemplate<In, Context> {
+        override fun IModelQLTemplateBuilder<In, Context>.applyLater(): IModelQLTemplateInstance<Context> {
+            return body()
+        }
+    }
+}
+
+class RequestedValue<E>(val step: IMonoStep<E>) {
+    private var initialized: Boolean = false
+    private var result: E? = null
+    fun set(value: E) {
+        result = value
+        initialized = true
+    }
+    fun get(): E {
+        require(initialized) { "Value not received for $this" }
+        return result as E
+    }
+}
+
+interface IModelQLTemplateBuilder<In, Context> {
+    val input: IMonoStep<In>
+    fun templateBody(body: Context.() -> Unit)
+    fun <T> IMonoStep<T>.prepare(): RequestedValue<T>
+    fun <TIn, TContext> IMonoStep<TIn>.prepareTemplate(template: IModelQLTemplate<TIn, TContext>): IModelQLTemplateInstance<TContext>
+    fun <TIn, TContext> IFluxStep<TIn>.prepareTemplate(template: IModelQLTemplate<TIn, TContext>): IModelQLTemplateInstanceList<TContext>
+    fun <ChildContext> ChildContext.applyTemplate(templateInstance: IModelQLTemplateInstance<ChildContext>)
+    fun <ChildContext> ChildContext.applyTemplates(templateInstance: IModelQLTemplateInstanceList<ChildContext>)
+}
+
+class ModelQLTemplateBuilder<In, Context>(val input: IMonoStep<In>) : IModelQLTemplateBuilder<In, Context>, IModelQLTemplateInstance<Context> {
+    private var templateBody: (Context.() -> Unit)? = null
+    private val valueRequests = ArrayList<RequestedValue<Any?>>()
+
+    override fun templateBody(body: Context.() -> Unit) {
+        this.templateBody = body
+    }
+    override fun applyTemplateInstance(context: Context) {
+        with(context) {
+            templateBody()
+        }
+    }
+    override fun <ChildContext> ChildContext.applyTemplate(templateInstance: IModelQLTemplateInstance<ChildContext>) {
+        templateInstance.applyTemplate(this)
+    }
+    override fun <ChildContext> ChildContext.applyTemplates(templateInstance: IModelQLTemplateInstanceList<ChildContext>) {
+        templateInstance.applyTemplates(this)
+    }
+    override fun <T> IMonoStep<T>.prepare(): RequestedValue<T> {
+        return RequestedValue<T>(this).also { valueRequests.add(it as RequestedValue<Any?>) }
+    }
+    override fun <TIn, TContext> IFluxStep<TIn>.prepareTemplate(template: IModelQLTemplate<TIn, TContext>): IModelQLTemplateInstance<TContext> {
+        toList()
+    }
+    override fun <TIn, TContext> IMonoStep<TIn>.prepareTemplate(template: IModelQLTemplate<TIn, TContext>): IModelQLTemplateInstance<TContext> {
+        val childBuilder = ModelQLTemplateBuilder<TIn, TContext>(this)
+        with(childBuilder) {
+            with(template) {
+                applyLater()
+            }
+        }
+        return object : IModelQLTemplateInstance<TContext> {
+            override fun applyTemplate(context: TContext) {
+                with(context) {
+                    return childBuilder.templateBody!!()
+                }
+            }
+        }
+    }
+}
+
+/**
+ * To be implemented by the user.
+ */
+interface IModelQLTemplate<In, Context> {
+    fun IModelQLTemplateBuilder<In, Context>.applyLater(): IModelQLTemplateInstance<Context>
+}
+
+interface IModelQLTemplateInstance<Context> {
+    fun applyTemplateInstance(context: Context)
+}
+interface IModelQLTemplateInstanceList<Context> {
+    fun applyTemplates(context: Context)
 }

4466a55f99d61d6842dc0f993d814fc7de2d6aa7
Michael Huster <mhuster@itemis.com>
2023-08-25 15:44:17 +0200

    fix(mps-model-adapters): fix init of RepositoryLanguage

--- mps-model-adapters/src/main/kotlin/org/modelix/model/mpsadapters/RepositoryLanguage.kt
+++ mps-model-adapters/src/main/kotlin/org/modelix/model/mpsadapters/RepositoryLanguage.kt
@@ -18,6 +18,9 @@ import jetbrains.mps.smodel.adapter.structure.concept.SInterfaceConceptAdapterBy
 import org.modelix.model.api.SimpleChildLink
 import org.modelix.model.api.SimpleConcept
 import org.modelix.model.api.SimpleLanguage
+import org.modelix.model.mpsadapters.RepositoryLanguage.BaseConcept
+import org.modelix.model.mpsadapters.RepositoryLanguage.INamedConcept
+import org.modelix.model.mpsadapters.RepositoryLanguage.addConcept
 
 object RepositoryLanguage : SimpleLanguage("org.modelix.model.repositoryconcepts", uid = "mps:0a7577d1-d4e5-431d-98b1-fae38f9aee80") {
     val BaseConcept = MPSConcept(SConceptAdapterById.deserialize("c:ceab5195-25ea-4f22-9b92-103b95ca8c0c/1133920641626:jetbrains.mps.lang.core.structure.BaseConcept"))
@@ -25,48 +28,52 @@ object RepositoryLanguage : SimpleLanguage("org.modelix.model.repositoryconcepts
     val NamePropertyUID = "ceab5195-25ea-4f22-9b92-103b95ca8c0c/1169194658468/1169194664001"
     val VirtualPackagePropertyUID = "ceab5195-25ea-4f22-9b92-103b95ca8c0c/1133920641626/1193676396447"
 
-    object Model : SimpleConcept(
-        conceptName = "Model",
-        uid = "mps:0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618892",
-        directSuperConcepts = listOf(BaseConcept, INamedConcept),
-    ) {
-        init { addConcept(this) }
-        val rootNodes = SimpleChildLink(
-            simpleName = "rootNodes",
-            isMultiple = true,
-            isOptional = true,
-            targetConcept = BaseConcept,
-            uid = "0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618892/474657388638618900",
-        )
-    }
+    val Model = org.modelix.model.mpsadapters.Model
+    val Module = org.modelix.model.mpsadapters.Module
+    val Repository = org.modelix.model.mpsadapters.Repository
+}
 
-    object Module : SimpleConcept(
-        conceptName = "Module",
-        uid = "mps:0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618895",
-        directSuperConcepts = listOf(BaseConcept, INamedConcept),
-    ) {
-        init { addConcept(this) }
-        val models = SimpleChildLink(
-            simpleName = "models",
-            isMultiple = true,
-            isOptional = true,
-            targetConcept = Model,
-            uid = "0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618895/474657388638618898",
-        )
-    }
+object Model : SimpleConcept(
+    conceptName = "Model",
+    uid = "mps:0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618892",
+    directSuperConcepts = listOf(BaseConcept, INamedConcept),
+) {
+    init { addConcept(this) }
+    val rootNodes = SimpleChildLink(
+        simpleName = "rootNodes",
+        isMultiple = true,
+        isOptional = true,
+        targetConcept = BaseConcept,
+        uid = "0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618892/474657388638618900",
+    )
+}
 
-    object Repository : SimpleConcept(
-        conceptName = "Repository",
-        uid = "mps:0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618902",
-        directSuperConcepts = listOf(BaseConcept),
-    ) {
-        init { addConcept(this) }
-        val modules = SimpleChildLink(
-            simpleName = "modules",
-            isMultiple = true,
-            isOptional = true,
-            targetConcept = Module,
-            uid = "0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618902/474657388638618903",
-        )
-    }
+object Module : SimpleConcept(
+    conceptName = "Module",
+    uid = "mps:0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618895",
+    directSuperConcepts = listOf(BaseConcept, INamedConcept),
+) {
+    init { addConcept(this) }
+    val models = SimpleChildLink(
+        simpleName = "models",
+        isMultiple = true,
+        isOptional = true,
+        targetConcept = Model,
+        uid = "0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618895/474657388638618898",
+    )
+}
+
+object Repository : SimpleConcept(
+    conceptName = "Repository",
+    uid = "mps:0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618902",
+    directSuperConcepts = listOf(BaseConcept),
+) {
+    init { addConcept(this) }
+    val modules = SimpleChildLink(
+        simpleName = "modules",
+        isMultiple = true,
+        isOptional = true,
+        targetConcept = Module,
+        uid = "0a7577d1-d4e5-431d-98b1-fae38f9aee80/474657388638618902/474657388638618903",
+    )
 }

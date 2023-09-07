84290a02ae8da51646ecfe098fe3e7b70f96285d
Michael Huster <mhuster@itemis.com>
2023-09-07 10:21:43 +0200

    test(bulk-model-sync-gradle): use build dir as sourceset instead of copying

--- bulk-model-sync-gradle-test/graph-lang-api/build.gradle.kts
+++ bulk-model-sync-gradle-test/graph-lang-api/build.gradle.kts
@@ -15,18 +15,12 @@ val modelixCoreVersion = file("../../version.txt").readText()
 
 version = modelixCoreVersion
 
-val mps by configurations.creating
+val mps: Configuration by configurations.creating
 val kotlinGenDir = buildDir.resolve("metamodel/kotlin").apply { mkdirs() }
 
 dependencies {
     mps("com.jetbrains:mps:2021.1.4")
     api("org.modelix:model-api-gen-runtime:$modelixCoreVersion")
-}
-
-val copyGeneratedApiToSrc by tasks.registering(Sync::class) {
-    dependsOn(tasks.named("generateMetaModelSources"))
-    from(kotlinGenDir)
-    into("src/main/kotlin")
 }
 
 val mpsDir = buildDir.resolve("mps").apply { mkdirs() }
@@ -41,6 +35,12 @@ val repoDir = projectDir.resolve("test-repo")
 val copyMetamodelToMpsHome by tasks.registering(Copy::class) {
     from(file(projectDir.resolve("../test-repo/languages")))
     into(file(mpsDir.resolve("languages").apply { mkdirs() }))
+}
+
+kotlin {
+    sourceSets.named("main") {
+        kotlin.srcDir(kotlinGenDir)
+    }
 }
 
 metamodel {
@@ -63,6 +63,10 @@ publishing {
     }
 }
 
+tasks.named("processResources") {
+    dependsOn("generateMetaModelSources")
+}
+
 tasks.named("compileKotlin") {
-    dependsOn(copyGeneratedApiToSrc)
+    dependsOn("generateMetaModelSources")
 }

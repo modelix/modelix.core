5eeaa4217ca0e2178ed5be33aacddd3165288b87
Benedek Horváth <bhorvath@itemis.com>
2023-09-07 17:05:12 +0200

    feat(sync-plugin-lib): ModelBinding is ready to test

--- sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/binding/ModelBinding.kt
+++ sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/binding/ModelBinding.kt
@@ -16,32 +16,400 @@
 
 package org.modelix.mps.sync.binding
 
+import gnu.trove.set.hash.TLongHashSet
+import jetbrains.mps.smodel.SModelInternal
+import jetbrains.mps.smodel.event.SModelChildEvent
+import jetbrains.mps.smodel.event.SModelDevKitEvent
+import jetbrains.mps.smodel.event.SModelImportEvent
+import jetbrains.mps.smodel.event.SModelLanguageEvent
+import jetbrains.mps.smodel.event.SModelListener
+import jetbrains.mps.smodel.event.SModelPropertyEvent
+import jetbrains.mps.smodel.event.SModelReferenceEvent
+import jetbrains.mps.smodel.event.SModelRenamedEvent
+import jetbrains.mps.smodel.event.SModelRootEvent
+import jetbrains.mps.smodel.loading.ModelLoadingState
+import org.jetbrains.mps.openapi.event.SNodeAddEvent
+import org.jetbrains.mps.openapi.event.SNodeRemoveEvent
+import org.jetbrains.mps.openapi.event.SPropertyChangeEvent
+import org.jetbrains.mps.openapi.event.SReferenceChangeEvent
 import org.jetbrains.mps.openapi.model.SModel
+import org.jetbrains.mps.openapi.model.SNodeChangeListener
 import org.modelix.model.api.ITree
 import org.modelix.model.api.ITreeChangeVisitor
+import org.modelix.model.api.ITreeChangeVisitorEx
 import org.modelix.model.api.IWriteTransaction
+import org.modelix.model.area.PArea
+import org.modelix.model.operations.RoleInNode
 import org.modelix.mps.sync.synchronization.SyncDirection
 
-class ModelBinding(val modelNodeId: Long, val model: SModel, initialSyncDirection: SyncDirection) :
+// status: ready to test
+class ModelBinding(val modelNodeId: Long, private val model: SModel, initialSyncDirection: SyncDirection) :
     BaseBinding(initialSyncDirection) {
 
+    private val logger = mu.KotlinLogging.logger {}
+
+    private val childrenSyncToMPSRequired: MutableSet<RoleInNode> = mutableSetOf()
+    private val referenceSyncToMPSRequired: MutableSet<RoleInNode> = mutableSetOf()
+    private val propertySyncToMPSRequired: MutableSet<RoleInNode> = mutableSetOf()
+    private val fullNodeSyncToMPSRequired: TLongHashSet = TLongHashSet()
+    private var modelPropertiesSyncToMPSRequired = false
+    private var synchronizer: ModelSynchronizer? = null
+
+    private val nodeChangeListener = object : SNodeChangeListener {
+        private val logger = mu.KotlinLogging.logger {}
+
+        override fun propertyChanged(event: SPropertyChangeEvent) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+
+                getBranch()?.let {
+                    PArea(it).executeWrite {
+                        synchronizer?.runAndFlushReferences {
+                            val transaction = it.writeTransaction
+                            val id = synchronizer?.getOrSyncToCloud(event.node, transaction)
+                            if (id != null && id != 0L && transaction.containsNode(id)) {
+                                transaction.setProperty(id, event.property.name, event.newValue)
+                            }
+                        }
+                    }
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+
+        override fun referenceChanged(event: SReferenceChangeEvent) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+                synchronizer?.runAndFlushReferences {
+                    synchronizer?.handleReferenceChanged(event)
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+
+        override fun nodeAdded(event: SNodeAddEvent) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+                synchronizer?.runAndFlushReferences {
+                    synchronizer?.handleMPSNodeAdded(event)
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+
+        override fun nodeRemoved(event: SNodeRemoveEvent) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+                synchronizer?.runAndFlushReferences {
+                    synchronizer?.handleMPSNodeRemoved(event)
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+    }
+
+    private val modelListener = object : SModelListener {
+        private val logger = mu.KotlinLogging.logger {}
+
+        override fun languageAdded(event: SModelLanguageEvent?) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+                synchronizer?.runAndFlushReferences {
+                    synchronizer?.syncUsedLanguagesAndDevKitsFromMPS()
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+
+        override fun languageRemoved(event: SModelLanguageEvent?) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+                synchronizer?.runAndFlushReferences {
+                    synchronizer?.syncUsedLanguagesAndDevKitsFromMPS()
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+
+        override fun devkitAdded(event: SModelDevKitEvent?) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+                synchronizer?.runAndFlushReferences {
+                    synchronizer?.syncUsedLanguagesAndDevKitsFromMPS()
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+
+        override fun devkitRemoved(event: SModelDevKitEvent?) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+                synchronizer?.runAndFlushReferences {
+                    synchronizer?.syncUsedLanguagesAndDevKitsFromMPS()
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+
+        override fun beforeChildRemoved(event: SModelChildEvent?) {}
+        override fun beforeModelDisposed(sm: SModel?) {}
+        override fun beforeModelRenamed(event: SModelRenamedEvent?) {}
+        override fun beforeRootRemoved(event: SModelRootEvent?) {}
+        override fun childAdded(event: SModelChildEvent?) {}
+        override fun childRemoved(event: SModelChildEvent?) {}
+
+        override fun getPriority(): SModelListener.SModelListenerPriority = SModelListener.SModelListenerPriority.CLIENT
+
+        override fun importAdded(event: SModelImportEvent?) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+                synchronizer?.runAndFlushReferences {
+                    synchronizer?.syncModelImportsFromMPS()
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+
+        override fun importRemoved(event: SModelImportEvent?) {
+            try {
+                if (isSynchronizing()) {
+                    return
+                }
+                synchronizer?.runAndFlushReferences {
+                    synchronizer?.syncModelImportsFromMPS()
+                }
+            } catch (ex: Exception) {
+                logger.error(ex) { ex.message }
+            }
+        }
+
+        override fun modelLoadingStateChanged(sm: SModel?, newState: ModelLoadingState?) {}
+        override fun modelRenamed(event: SModelRenamedEvent?) {}
+        override fun modelSaved(sm: SModel?) {}
+        override fun propertyChanged(event: SModelPropertyEvent?) {}
+        override fun referenceAdded(event: SModelReferenceEvent?) {}
+        override fun referenceRemoved(event: SModelReferenceEvent?) {}
+
+        @Deprecated("Deprecated in Java")
+        override fun rootAdded(event: SModelRootEvent?) {
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun rootRemoved(event: SModelRootEvent?) {
+        }
+    }
+
     override fun doActivate() {
-        TODO("Not yet implemented")
+        synchronizer = ModelSynchronizer(modelNodeId, model, getCloudRepository()!!)
+        model.addChangeListener(nodeChangeListener)
+        (model as SModelInternal).addModelListener(modelListener)
     }
 
     override fun doDeactivate() {
-        TODO("Not yet implemented")
-    }
-
-    override fun getTreeChangeVisitor(oldTree: ITree?, newTree: ITree?): ITreeChangeVisitor? {
-        TODO("Not yet implemented")
+        model.removeChangeListener(nodeChangeListener)
+        (model as SModelInternal).removeModelListener(modelListener)
+        synchronizer = null
     }
 
     override fun doSyncToCloud(transaction: IWriteTransaction) {
-        TODO("Not yet implemented")
+        synchronizer?.fullSyncFromMPS()
     }
 
     override fun doSyncToMPS(tree: ITree) {
-        TODO("Not yet implemented")
+        if (runningTask?.isInitialSync == true) {
+            val mpsRootNodes = model.rootNodes
+            // TODO instead of "rootNodes" it must be link/Model: rootNodes/.getName()
+            val cloudRootNodes = tree.getChildren(modelNodeId, "rootNodes")
+            val mpsRootNodesIsNotEmpty = mpsRootNodes.firstOrNull() != null
+            val cloudRootNodesIsEmpty = cloudRootNodes.firstOrNull() == null
+            if (mpsRootNodesIsNotEmpty && cloudRootNodesIsEmpty) {
+                // TODO remove this workaround
+                forceEnqueueSyncTo(SyncDirection.TO_CLOUD, true, null)
+            } else {
+                synchronizer?.syncModelToMPS(tree, false)
+            }
+        } else {
+            synchronizer?.runAndFlushReferences {
+                childrenSyncToMPSRequired.forEach { roleInNode ->
+                    try {
+                        if (tree.containsNode(roleInNode.nodeId)) {
+                            synchronizer?.syncChildrenToMPS(roleInNode.nodeId, roleInNode.role, tree, false)
+                        }
+                    } catch (ex: Exception) {
+                        logger.error(ex) { ex.message }
+                    }
+                }
+                childrenSyncToMPSRequired.clear()
+
+                referenceSyncToMPSRequired.forEach { roleInNode ->
+                    try {
+                        if (tree.containsNode(roleInNode.nodeId)) {
+                            synchronizer?.syncReferenceToMPS(roleInNode.nodeId, roleInNode.role, tree)
+                        }
+                    } catch (ex: Exception) {
+                        logger.error(ex) { ex.message }
+                    }
+                }
+                referenceSyncToMPSRequired.clear()
+
+                propertySyncToMPSRequired.forEach { roleInNode ->
+                    try {
+                        if (tree.containsNode(roleInNode.nodeId)) {
+                            synchronizer?.syncPropertyToMPS(roleInNode.nodeId, roleInNode.role, tree)
+                        }
+                    } catch (ex: Exception) {
+                        logger.error(ex) { ex.message }
+                    }
+                }
+                propertySyncToMPSRequired.clear()
+
+                fullNodeSyncToMPSRequired.forEach { nodeId ->
+                    try {
+                        if (tree.containsNode(nodeId)) {
+                            synchronizer?.syncNodeToMPS(nodeId, tree, true)
+                        }
+                    } catch (ex: Exception) {
+                        logger.error(ex) { ex.message }
+                    }
+                    true
+                }
+                fullNodeSyncToMPSRequired.clear()
+
+                if (modelPropertiesSyncToMPSRequired) {
+                    try {
+                        synchronizer?.syncModelPropertiesToMPS(tree)
+                    } catch (ex: Exception) {
+                        logger.error(ex) { ex.message }
+                    }
+                }
+            }
+            modelPropertiesSyncToMPSRequired = false
+        }
     }
+
+    override fun getTreeChangeVisitor(oldTree: ITree?, newTree: ITree?): ITreeChangeVisitor {
+        return object : ITreeChangeVisitorEx {
+            private fun isInsideModel(nodeId: Long): Boolean {
+                assertSyncThread()
+                val parent = newTree?.getParent(nodeId)!!
+                if (parent == 0L) {
+                    return false
+                }
+                // TODO instead of "rootNodes" it must be link/Model : rootNodes/.getName()
+                if (parent == modelNodeId) {
+                    return newTree.getRole(nodeId) == "rootNodes"
+                }
+                return isInsideModel(parent)
+            }
+
+            private fun isInsideModelOrModel(nodeId: Long): Boolean {
+                assertSyncThread()
+                return if (nodeId == modelNodeId) {
+                    true
+                } else {
+                    isInsideModel(nodeId)
+                }
+            }
+
+            private fun isModelProperties(nodeId: Long): Boolean {
+                assertSyncThread()
+                val parent = newTree?.getParent(nodeId)!!
+                if (parent == 0L) {
+                    return false
+                }
+                // TODO instead of "rootNodes" it must be link/Model : rootNodes/.getName()
+                if (parent == modelNodeId) {
+                    return newTree.getRole(nodeId) != "rootNodes"
+                }
+                return isModelProperties(parent)
+            }
+
+            override fun containmentChanged(nodeId: Long) {}
+
+            override fun childrenChanged(nodeId: Long, role: String?) {
+                assertSyncThread()
+                if (modelNodeId == nodeId) {
+                    // TODO instead of "rootNodes" it must be link/Model : rootNodes/.getName()
+                    if (role == "rootNodes") {
+                        childrenSyncToMPSRequired.add(RoleInNode(nodeId, role))
+                    } else {
+                        modelPropertiesSyncToMPSRequired = true
+                    }
+                } else if (isModelProperties(nodeId)) {
+                    modelPropertiesSyncToMPSRequired = true
+                } else if (isInsideModel(nodeId)) {
+                    childrenSyncToMPSRequired.add(RoleInNode(nodeId, role))
+                }
+                enqueueSync(SyncDirection.TO_MPS, false, null)
+            }
+
+            override fun referenceChanged(nodeId: Long, role: String) {
+                assertSyncThread()
+                if (isModelProperties(nodeId)) {
+                    modelPropertiesSyncToMPSRequired = true
+                    enqueueSync(SyncDirection.TO_MPS, false, null)
+                    return
+                }
+                if (!isInsideModel(nodeId)) {
+                    return
+                }
+                referenceSyncToMPSRequired.add(RoleInNode(nodeId, role))
+                enqueueSync(SyncDirection.TO_MPS, false, null)
+            }
+
+            override fun propertyChanged(nodeId: Long, role: String) {
+                assertSyncThread()
+                if (isModelProperties(nodeId)) {
+                    modelPropertiesSyncToMPSRequired = true
+                    enqueueSync(SyncDirection.TO_MPS, false, null)
+                    return
+                }
+                if (!isInsideModel(nodeId)) {
+                    return
+                }
+                propertySyncToMPSRequired.add(RoleInNode(nodeId, role))
+                enqueueSync(SyncDirection.TO_MPS, false, null)
+            }
+
+            override fun nodeRemoved(nodeId: Long) {}
+
+            override fun nodeAdded(nodeId: Long) {
+                assertSyncThread()
+                if (!isInsideModel(nodeId)) {
+                    return
+                }
+                fullNodeSyncToMPSRequired.add(nodeId)
+                enqueueSync(SyncDirection.TO_MPS, false, null)
+            }
+        }
+    }
+
+    override fun toString(): String = "Model: ${java.lang.Long.toHexString(modelNodeId)} -> ${model.name.value}"
 }
--- sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/binding/ModelSynchronizer.kt
+++ sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/binding/ModelSynchronizer.kt
@@ -16,9 +16,179 @@
 
 package org.modelix.mps.sync.binding
 
-class ModelSynchronizer {
+import jetbrains.mps.project.ModelImporter
+import org.jetbrains.mps.openapi.event.SNodeAddEvent
+import org.jetbrains.mps.openapi.event.SNodeRemoveEvent
+import org.jetbrains.mps.openapi.event.SReferenceChangeEvent
+import org.jetbrains.mps.openapi.model.SModel
+import org.jetbrains.mps.openapi.model.SNode
+import org.modelix.model.api.ITree
+import org.modelix.model.api.IWriteTransaction
+import org.modelix.mps.sync.ICloudRepository
+import org.modelix.mps.sync.synchronization.SyncDirection
+import org.modelix.mps.sync.synchronization.Synchronizer
+
+class ModelSynchronizer(
+    private val modelNodeId: Long,
+    private val model: SModel,
+    private val cloudRepository: ICloudRepository,
+) {
     companion object {
         val USED_DEVKITS = "usedDevkits"
         val MPS_NODE_ID_PROPERTY_NAME = "#mpsNodeId#"
+    }
+
+    private val pendingReferences: PendingReferences = PendingReferences()
+
+    protected val nodeMap = NodeMap { this.getBranch() }
+
+    fun getBranch() = cloudRepository.getBranch()
+
+    fun runAndFlushReferences(runnable: Runnable) = pendingReferences.runAndFlush(runnable)
+
+    fun getOrSyncToCloud(node: SNode, transaction: IWriteTransaction): Long {
+        var cloudId = nodeMap.getId(node)
+        if (cloudId == 0L || !transaction.containsNode(cloudId)) {
+            val parent = node.parent
+            if (parent == null) {
+                syncRootNodesFromMPS()
+            } else {
+                val parentCloudId = getOrSyncToCloud(parent, transaction)
+                syncNodeFromMPS(parent, true)
+            }
+            cloudId = nodeMap.getId(node)
+        }
+        return cloudId
+    }
+
+    private fun syncRootNodesFromMPS() {
+        val transaction = getBranch().writeTransaction
+        // TODO instead of "rootNodes" it must be link/Model: rootNodes/.getName()
+        val syncedNodes = createChildrenSynchronizer(modelNodeId, "rootNodes").syncToCloud(transaction)
+        syncedNodes.forEach {
+            syncNodeFromMPS(it.value, true)
+        }
+    }
+
+    private fun syncNodeFromMPS(parentNode: SNode, includeDescendants: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    private fun createChildrenSynchronizer(parentId: Long, role: String): Synchronizer<SNode> {
+        return object : Synchronizer<SNode>(parentId, role) {
+            override fun getMPSChildren(): Iterable<SNode> {
+                TODO("Not yet implemented")
+            }
+
+            override fun createMPSChild(tree: ITree, cloudChildId: Long): SNode? {
+                TODO("Not yet implemented")
+            }
+
+            override fun associate(
+                tree: ITree,
+                cloudChildren: List<Long>,
+                mpsChildren: List<SNode>,
+                direction: SyncDirection,
+            ): MutableMap<Long, SNode> {
+                TODO("Not yet implemented")
+            }
+
+            override fun removeMPSChild(mpsChild: SNode) {
+                TODO("Not yet implemented")
+            }
+
+            override fun createCloudChild(transaction: IWriteTransaction, mpsChild: SNode): Long {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    fun handleReferenceChanged(event: SReferenceChangeEvent) {
+        TODO("Not yet implemented")
+    }
+
+    fun handleMPSNodeAdded(event: SNodeAddEvent) {
+        TODO("Not yet implemented")
+    }
+
+    fun handleMPSNodeRemoved(event: SNodeRemoveEvent) {
+        TODO("Not yet implemented")
+    }
+
+    fun syncUsedLanguagesAndDevKitsFromMPS() {
+        TODO("Not yet implemented")
+    }
+
+    fun syncModelImportsFromMPS() {
+        TODO("Not yet implemented")
+    }
+
+    fun fullSyncFromMPS() {
+        TODO("Not yet implemented")
+    }
+
+    fun syncModelToMPS(tree: ITree, withInitialRemoval: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    fun syncChildrenToMPS(nodeId: Long, role: String?, tree: ITree, includeDescendants: Boolean) {
+    }
+
+    fun syncReferenceToMPS(nodeId: Long, role: String?, tree: ITree) {
+    }
+
+    fun syncPropertyToMPS(nodeId: Long, role: String?, tree: ITree) {
+    }
+
+    fun syncNodeToMPS(nodeId: Long, tree: ITree, includeDescendants: Boolean) {
+    }
+
+    fun syncModelPropertiesToMPS(tree: ITree) {
+    }
+
+    inner class PendingReferences {
+        private val logger = mu.KotlinLogging.logger {}
+
+        private var currentReferences: MutableList<() -> SNode>? = null
+
+        fun runAndFlush(runnable: Runnable) {
+            synchronized(this) {
+                currentReferences?.let {
+                    runnable.run()
+                    return
+                }
+
+                try {
+                    currentReferences = mutableListOf()
+                    runnable.run()
+                } finally {
+                    try {
+                        processPendingReferences()
+                    } catch (ex: Exception) {
+                        logger.error(ex) { "Failed to process pending reference" }
+                    }
+                    currentReferences = null
+                }
+            }
+        }
+
+        private fun processPendingReferences() {
+            val targetModels = mutableSetOf<SModel?>()
+            currentReferences!!.forEach {
+                try {
+                    val targetNode = it.invoke()
+                    targetModels.add(targetNode.model)
+                } catch (ex: Exception) {
+                    logger.error(ex) { ex.message }
+                }
+            }
+
+            val modelsToImport = targetModels.filter { it != null && it != model }
+            if (modelsToImport.isNotEmpty()) {
+                val importer = ModelImporter(model)
+                modelsToImport.forEach { importer.prepare(it?.reference) }
+                importer.execute()
+            }
+        }
     }
 }

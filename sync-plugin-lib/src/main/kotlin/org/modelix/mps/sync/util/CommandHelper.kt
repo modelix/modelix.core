05309ef801c15c0ab011b9e3e6bec4bb72c74685
Benedek Horváth <bhorvath@itemis.com>
2023-09-12 14:25:07 +0200

    feat(sync-plugin-lib): migrate CommandHelper

--- sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/util/CommandHelper.kt
+++ sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/util/CommandHelper.kt
@@ -16,7 +16,96 @@
 
 package org.modelix.mps.sync.util
 
+import com.intellij.openapi.application.ApplicationManager
+import jetbrains.mps.ide.ThreadUtils
+import jetbrains.mps.project.IProject
+import jetbrains.mps.project.ProjectManager
+import jetbrains.mps.smodel.MPSModuleRepository
+import okhttp3.internal.toImmutableList
+import org.jetbrains.mps.openapi.module.SRepository
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import javax.swing.Timer
+
+// status: ready to test
 object CommandHelper {
-    fun runInUndoTransparentCommand(runnable: Runnable) {
+
+    private val logger = mu.KotlinLogging.logger {}
+
+    private val queue = mutableListOf<Pair<Runnable, Boolean>>()
+
+    private val timerActionEvent = object : ActionListener {
+        override fun actionPerformed(e: ActionEvent?) {
+            if (project == null) {
+                return
+            }
+            timer.stop()
+            val queueCopy = queue.toImmutableList()
+            queue.clear()
+            queueCopy.forEach { entry ->
+                try {
+                    executeCommand(project!!.repository, entry.second, entry.first)
+                } catch (ex: Exception) {
+                    logger.error(ex) { ex.message }
+                }
+            }
+        }
+    }
+
+    private val timer: Timer = Timer(10, timerActionEvent)
+
+    private val project: IProject?
+        get() = ProjectManager.getInstance().openedProjects.firstOrNull()
+
+    fun runInCommand(runnable: Runnable) = runInCommand(runnable, false)
+
+    fun runInUndoTransparentCommand(runnable: Runnable) = runInCommand(runnable, true)
+
+    private fun runInCommand(runnable: Runnable, undoTransparent: Boolean) {
+        if (project == null) {
+            queue.add(Pair(runnable, undoTransparent))
+            if (!timer.isRunning) {
+                timer.start()
+            }
+        } else {
+            var ex: Throwable? = null
+            val runnableWithExceptionHandling = {
+                try {
+                    runnable.run()
+                } catch (t: Throwable) {
+                    ex = t
+                }
+            }
+            if (ThreadUtils.isInEDT()) {
+                executeCommand(project!!.repository, undoTransparent, runnableWithExceptionHandling)
+            } else {
+                ApplicationManager.getApplication().invokeAndWait {
+                    executeCommand(project!!.repository, undoTransparent, runnableWithExceptionHandling)
+                }
+            }
+            if (ex != null) {
+                throw RuntimeException("Exception in command", ex)
+            }
+        }
+    }
+
+    private fun executeCommand(repository: SRepository, undoTransparent: Boolean, runnable: Runnable) {
+        ThreadUtils.assertEDT()
+        val modelAccess = repository.modelAccess
+        if (modelAccess.canWrite()) {
+            runnable.run()
+        } else {
+            if (undoTransparent) {
+                modelAccess.executeUndoTransparentCommand(runnable)
+            } else {
+                modelAccess.executeCommand(runnable)
+            }
+        }
+    }
+
+    fun getSRepository(): SRepository {
+        val openedProjects = ProjectManager.getInstance().openedProjects
+        val projectRepo = openedProjects.firstNotNullOfOrNull { it.repository }
+        return projectRepo ?: MPSModuleRepository.getInstance()
     }
 }

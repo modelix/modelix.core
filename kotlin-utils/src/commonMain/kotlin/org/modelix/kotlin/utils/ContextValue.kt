b1c1310afefe20463f8c0ddb5df161903fa66b63
Sascha Lisson <sascha.lisson@itemis.de>
2023-09-13 11:35:52 +0200

    docs: API documentation for ContextValue

--- kotlin-utils/src/commonMain/kotlin/org/modelix/kotlin/utils/ContextValue.kt
+++ kotlin-utils/src/commonMain/kotlin/org/modelix/kotlin/utils/ContextValue.kt
@@ -13,11 +13,20 @@
  */
 package org.modelix.kotlin.utils
 
+/**
+ * A common abstraction over ThreadLocal and CoroutineContext that integrates both worlds.
+ * Allows to set a value that can be read from everywhere on the current thread or coroutine.
+ * A suspendable function can call non suspendable functions and the value is synchronized between the CoroutineContext
+ * and the internal ThreadLocal.
+ */
 expect class ContextValue<E> {
 
     constructor()
     constructor(defaultValue: E)
 
+    /**
+     * @throws NoSuchElementException if no value is set.
+     */
     fun getValue(): E
     fun getValueOrNull(): E?
     fun getAllValues(): List<E>

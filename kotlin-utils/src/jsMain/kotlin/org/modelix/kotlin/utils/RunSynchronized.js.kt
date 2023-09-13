a4053e22ebed437a23ac74eb72fb58a90dfa1859
Sascha Lisson <sascha.lisson@itemis.de>
2023-09-13 11:54:15 +0200

    chore: explanation why runSynchronized does nothing on JS

--- kotlin-utils/src/jsMain/kotlin/org/modelix/kotlin/utils/RunSynchronized.js.kt
+++ kotlin-utils/src/jsMain/kotlin/org/modelix/kotlin/utils/RunSynchronized.js.kt
@@ -17,5 +17,6 @@
 package org.modelix.kotlin.utils
 
 actual inline fun <R> runSynchronized(lock: Any, block: () -> R): R {
+    // This method is only required when compiling to the JVM. JS is single-threaded.
     return block()
 }

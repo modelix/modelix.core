616d32f06c8ab899f452d487b2f085f21f29f159
Benedek Horváth <bhorvath@itemis.com>
2023-09-22 10:17:54 +0200

    feat(sync-plugin-lib): migrated ModelServerConnections

--- sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/connection/ModelServerConnection.kt
+++ sync-plugin-lib/src/main/kotlin/org/modelix/mps/sync/connection/ModelServerConnection.kt
@@ -1,12 +1,19 @@
 package org.modelix.mps.sync.connection
 
 import org.modelix.model.api.IBranch
+import org.modelix.model.area.PArea
 import org.modelix.model.client.ActiveBranch
 import org.modelix.model.lazy.RepositoryId
+import org.modelix.mps.sync.CloudRepository
 import org.modelix.mps.sync.binding.RootBinding
 import java.net.URL
 
 class ModelServerConnection(var baseUrl: URL) {
+
+    companion object {
+        val uiStateRepositoryId = RepositoryId("uistate")
+    }
+
     fun getActiveBranch(repositoryId: RepositoryId): ActiveBranch {
         TODO("Not yet implemented")
     }
@@ -24,6 +31,38 @@ class ModelServerConnection(var baseUrl: URL) {
     }
 
     fun addBinding(repositoryId: RepositoryId, binding: Any) {
+        TODO("Not yet implemented")
+    }
+
+    fun getActiveBranches(): Iterable<ActiveBranch> {
+        TODO()
+    }
+
+    private fun getInfo(): Any {
+        // should return org.modelix.model.runtimelang.structure.ModelServerInfo
+        TODO()
+    }
+
+    fun trees(): Iterable<CloudRepository> {
+        val info = this.getInfo()
+        return PArea(this.getInfoBranch()).executeRead {
+            // We want to obtain a list within the transaction.
+            // A sequence (which is lazy) would not work
+
+            // TODO migrate if org.modelix.model.runtimelang.structure.ModelServerInfo is on the CP
+            /*
+            info.repositories.select({~it =>
+                RepositoryId repositoryId = new RepositoryId(it.id);
+                new CloudRepository(this, repositoryId);
+            }).toList;
+             */
+
+            val result: Iterable<CloudRepository> = null!!
+            result
+        }
+    }
+
+    fun dispose() {
         TODO("Not yet implemented")
     }
 

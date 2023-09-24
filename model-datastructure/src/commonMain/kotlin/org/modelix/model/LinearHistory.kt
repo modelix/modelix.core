4cd2cfd8c366141091d5ad29844f2aec5c1e2d7e
Sascha Lisson <sascha.lisson@itemis.de>
2023-09-24 20:41:58 +0200

    fix(model-datastructure): performance of LinearHistory
    Some of the tests in ReplicatedRepositoryTest took up to several
    minutes. Now they all finish a few seconds.
    
    Also added a test that runs in a single coroutine to get
    reproducible results.
    
    This actually fixes MODELIX-524

--- model-datastructure/src/commonMain/kotlin/org/modelix/model/LinearHistory.kt
+++ model-datastructure/src/commonMain/kotlin/org/modelix/model/LinearHistory.kt
@@ -5,7 +5,14 @@ import org.modelix.model.lazy.IDeserializingKeyValueStore
 import org.modelix.model.lazy.KVEntryReference
 import org.modelix.model.persistent.CPVersion
 
-class LinearHistory(val baseVersionHash: String?) {
+/**
+ * Was introduced in https://github.com/modelix/modelix/commit/19c74bed5921028af3ac3ee9d997fc1c4203ad44
+ * together with the UndoOp. The idea is that an undo should only revert changes if there is no other change that relies
+ * on it. In that case the undo should do nothing to not indirectly undo newer changes.
+ * For example, if you added a node and someone else started changing properties on the that node, your undo should not
+ * remove the node to not lose the property changes.
+ */
+class SlowLinearHistory(val baseVersionHash: String?) {
 
     val version2descendants: MutableMap<Long, MutableSet<Long>> = HashMap()
     val versions: MutableMap<Long, CLVersion> = HashMap()
@@ -21,7 +28,7 @@ class LinearHistory(val baseVersionHash: String?) {
         var result: List<Long> = ArrayList()
 
         for (version in versions.values.filter { !it.isMerge() }.sortedBy { it.id }) {
-            val descendantIds = version2descendants[version.id]!!.sorted()
+            val descendantIds = version2descendants[version.id]!!.sorted().toSet()
             val idsInResult = result.toHashSet()
             if (idsInResult.contains(version.id)) {
                 result =
@@ -53,6 +60,76 @@ class LinearHistory(val baseVersionHash: String?) {
             val previous = version.baseVersion
             if (previous != null) {
                 collect(previous, path + version)
+            }
+        }
+    }
+
+    private fun getVersion(hash: KVEntryReference<CPVersion>, store: IDeserializingKeyValueStore): CLVersion {
+        return CLVersion(hash.getValue(store), store)
+    }
+}
+
+class LinearHistory(val baseVersionHash: String?) {
+
+    val version2directDescendants: MutableMap<Long, Set<Long>> = HashMap()
+    val versions: MutableMap<Long, CLVersion> = HashMap()
+
+    /**
+     * Oldest version first
+     */
+    fun load(vararg fromVersions: CLVersion): List<CLVersion> {
+        for (fromVersion in fromVersions) {
+            collect(fromVersion, null)
+        }
+
+        var result: List<Long> = ArrayList()
+
+        for (version in versions.values.filter { !it.isMerge() }.sortedBy { it.id }) {
+            val descendantIds = HashSet<Long>().also { collectAllDescendants(version.id, it) }.filter { !versions[it]!!.isMerge() }.sorted().toSet()
+            val idsInResult = result.toHashSet()
+            if (idsInResult.contains(version.id)) {
+                result =
+                    result +
+                    descendantIds.filter { !idsInResult.contains(it) }
+            } else {
+                result =
+                    result.filter { !descendantIds.contains(it) } +
+                    version.id +
+                    result.filter { descendantIds.contains(it) } +
+                    descendantIds.filter { !idsInResult.contains(it) }
+            }
+        }
+        return result.map { versions[it]!! }
+    }
+
+    private fun collectAllDescendants(ancestor: Long, result: MutableSet<Long>, visited: MutableSet<Long> = HashSet()) {
+        if (!visited.add(ancestor)) return
+        val descendants = version2directDescendants[ancestor] ?: return
+
+        result += descendants
+        descendants.forEach {
+            collectAllDescendants(it, result, visited)
+        }
+    }
+
+    private fun collect(version: CLVersion, descendant: CLVersion?) {
+        if (version.getContentHash() == baseVersionHash) return
+        if (descendant != null) {
+            version2directDescendants[version.id] = (version2directDescendants[version.id] ?: emptySet()) + setOf(descendant.id)
+        }
+        if (versions.containsKey(version.id)) return
+
+        versions[version.id] = version
+
+        if (version.isMerge()) {
+            val version1 = getVersion(version.data!!.mergedVersion1!!, version.store)
+            val version2 = getVersion(version.data!!.mergedVersion2!!, version.store)
+            collect(version1, version)
+            collect(version2, version)
+        } else {
+            val previous = version.baseVersion
+            if (previous != null) {
+                collect(previous, version)
             }
         }
     }

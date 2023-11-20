bf205be2491d6c7d69903a06969891db628dfa19
Sascha Lisson <sascha.lisson@itemis.de>
2023-11-20 16:42:29 +0100

    fix(model-api-gen): concept.typed().untyped() failed for unknown concepts
    Concepts that weren't registered or had no generated typed API weren't
    handled properly when converting between typed and untyped APIs.

--- model-api-gen-runtime/src/commonMain/kotlin/org/modelix/metamodel/UnknownConcept.kt
+++ model-api-gen-runtime/src/commonMain/kotlin/org/modelix/metamodel/UnknownConcept.kt
@@ -7,7 +7,9 @@ import org.modelix.model.api.ILanguage
 import org.modelix.model.api.INode
 import org.modelix.model.api.IProperty
 import org.modelix.model.api.IReferenceLink
+import org.modelix.model.api.getConcept
 import org.modelix.model.area.IArea
+import kotlin.reflect.KClass
 
 abstract class EmptyConcept : IConcept {
     override fun isAbstract(): Boolean = true
@@ -77,15 +79,52 @@ data class UnknownConcept(private val ref: IConceptReference) : EmptyConcept() {
     override fun getLongName(): String = getShortName()
 }
 
-data class UnknownTypedConcept(private val ref: IConceptReference?) : ITypedConcept {
+data class UnknownTypedConcept(private val ref: IConcept?) : IConceptOfTypedNode<UnknownConceptInstance> {
     override fun untyped(): IConcept {
-        return ref?.let { UnknownConcept(it) } ?: NullConcept
+        return ref ?: NullConcept
+    }
+
+    override fun getInstanceInterface(): KClass<out UnknownConceptInstance> {
+        return UnknownConceptInstance::class
     }
 }
 
 data class UnknownConceptInstance(val node: INode) : ITypedNode {
     override val _concept: ITypedConcept
-        get() = UnknownTypedConcept(node.getConceptReference())
+        get() = UnknownTypedConcept(node.getConcept())
 
     override fun unwrap(): INode = node
+}
+
+abstract class UnknownTypedChildLink : ITypedChildLink<ITypedNode> {
+
+    override fun castChild(childNode: INode): ITypedNode {
+        return childNode.typed()
+    }
+
+    override fun getTypedChildConcept(): IConceptOfTypedNode<ITypedNode> {
+        return untyped().targetConcept.typed() as IConceptOfTypedNode<ITypedNode>
+    }
+}
+
+data class UnknownTypedSingleChildLink(private val link: IChildLink) : UnknownTypedChildLink(), ITypedSingleChildLink<ITypedNode> {
+    override fun untyped(): IChildLink = link
+}
+
+data class UnknownTypedChildLinkList(private val link: IChildLink) : UnknownTypedChildLink(), ITypedChildListLink<ITypedNode> {
+    override fun untyped(): IChildLink = link
+}
+
+data class UnknownTypedReferenceLink(private val link: IReferenceLink) : ITypedReferenceLink<ITypedNode> {
+    override fun untyped(): IReferenceLink {
+        return link
+    }
+
+    override fun castTarget(target: INode): ITypedNode {
+        return target.typed()
+    }
+
+    override fun getTypedTargetConcept(): IConceptOfTypedNode<ITypedNode> {
+        return untyped().targetConcept.typed() as IConceptOfTypedNode<ITypedNode>
+    }
 }

fffb45e92984bd305676c265d0cd963d63b8ed11
Sascha Lisson <sascha.lisson@itemis.de>
2023-11-20 16:44:09 +0100

    fix(model-api): added @Serializable to NodeReference
    To allow direct use in other serializable classes.

--- model-api/src/commonMain/kotlin/org/modelix/model/api/SerializedNodeReference.kt
+++ model-api/src/commonMain/kotlin/org/modelix/model/api/SerializedNodeReference.kt
@@ -1,8 +1,11 @@
 package org.modelix.model.api
 
+import kotlinx.serialization.Serializable
+
 @Deprecated("renamed to NodeReference", ReplaceWith("NodeReference"))
 typealias SerializedNodeReference = NodeReference
 
+@Serializable
 data class NodeReference(val serialized: String) : INodeReference {
     override fun serialize(): String = serialized
 }

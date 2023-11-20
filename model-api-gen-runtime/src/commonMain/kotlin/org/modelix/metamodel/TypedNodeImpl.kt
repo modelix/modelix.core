295264fbc4900b52b66af3fbb4e88fa083d37487
Sascha Lisson <sascha.lisson@itemis.de>
2023-11-20 16:49:41 +0100

    fix(model-api): deprecated IConcept.getConcept()
    It's an extension method that does an unnecessary resolution of the
    concept. The implementation INode.concept decides if the resolution is
    necessary or if the concept can be accessed in a more efficient way.

--- model-api-gen-runtime/src/commonMain/kotlin/org/modelix/metamodel/TypedNodeImpl.kt
+++ model-api-gen-runtime/src/commonMain/kotlin/org/modelix/metamodel/TypedNodeImpl.kt
@@ -2,13 +2,12 @@ package org.modelix.metamodel
 
 import org.modelix.model.api.IConcept
 import org.modelix.model.api.INode
-import org.modelix.model.api.getConcept
 
 abstract class TypedNodeImpl(val wrappedNode: INode) : ITypedNode {
 
     init {
         val expected: IConcept = _concept._concept
-        val actual: IConcept? = unwrap().getConcept()
+        val actual: IConcept? = unwrap().concept
         require(actual != null && actual.isSubConceptOf(expected)) {
             "Concept of node ${unwrap()} expected to be a sub-concept of $expected, but was $actual"
         }

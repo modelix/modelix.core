c495445522c61c711595f90dfc40602ecb3248ab
Johannes Wienke <jwienke@itemis.com>
2023-12-04 11:54:42 +0100

    chore: disable the commit message body line length limit
    This only breaks pasting URLs or long package names.

--- commitlint.config.js
+++ commitlint.config.js
@@ -27,6 +27,8 @@ module.exports = {
         "vue-model-api",
       ],
     ],
-    "subject-case": [0, 'never']
+    "subject-case": [0, 'never'],
+    // No need to restrict the body line length. That only gives issues with URLs etc.
+    "body-max-line-length": [0, 'always']
   },
 };

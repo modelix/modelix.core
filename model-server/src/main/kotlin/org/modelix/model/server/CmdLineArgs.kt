17f11d6377e9923de0bb103ca707e2433251bd1d
Sascha Lisson <sascha.lisson@itemis.de>
2024-01-16 11:50:00 +0100

    fix(model-server): use double dashes for command line parameters
    Single dashes are usually only used for the short single letter name of a parameter.
    Single dash parameters are now deprecated and shouldn't be used.

--- model-server/src/main/kotlin/org/modelix/model/server/CmdLineArgs.kt
+++ model-server/src/main/kotlin/org/modelix/model/server/CmdLineArgs.kt
@@ -8,42 +8,42 @@ import java.io.File
 import java.util.LinkedList
 
 internal class CmdLineArgs {
-    @Parameter(names = ["-secret"], description = "Path to the secretfile", converter = FileConverter::class)
+    @Parameter(names = ["-secret", "--secret"], description = "Path to the secretfile", converter = FileConverter::class)
     var secretFile = File("/secrets/modelsecret/modelsecret.txt")
 
     @Parameter(
-        names = ["-jdbcconf"],
+        names = ["-jdbcconf", "--jdbcconf"],
         description = "Path to the JDBC configuration file",
         converter = FileConverter::class,
     )
     var jdbcConfFile: File? = null
 
-    @Parameter(names = ["-inmemory"], description = "Use in-memory storage", converter = BooleanConverter::class)
+    @Parameter(names = ["-inmemory", "--inmemory"], description = "Use in-memory storage", converter = BooleanConverter::class)
     var inmemory = false
 
     @Parameter(names = ["--local-persistence"], description = "Use ignite with local disk persistence", converter = BooleanConverter::class)
     var localPersistence = false
 
-    @Parameter(names = ["-dumpout"], description = "Dump in memory storage", converter = StringConverter::class)
+    @Parameter(names = ["-dumpout", "--dumpout"], description = "Dump in memory storage", converter = StringConverter::class)
     var dumpOutName: String? = null
 
-    @Parameter(names = ["-dumpin"], description = "Read dump in memory storage", converter = StringConverter::class)
+    @Parameter(names = ["-dumpin", "--dumpin"], description = "Read dump in memory storage", converter = StringConverter::class)
     var dumpInName: String? = null
 
-    @Parameter(names = ["-port"], description = "Set port", converter = IntegerConverter::class)
+    @Parameter(names = ["-port", "--port"], description = "Set port", converter = IntegerConverter::class)
     var port: Int? = null
 
-    @Parameter(names = ["-set"], description = "Set values", arity = 2)
+    @Parameter(names = ["-set", "--set"], description = "Set values", arity = 2)
     var setValues: List<String> = LinkedList<String>()
 
     @Parameter(
-        names = ["-schemainit"],
+        names = ["-schemainit", "--schemainit"],
         description = "Initialize the schema, if necessary",
         converter = BooleanConverter::class,
     )
     var schemaInit = false
 
-    @Parameter(names = ["-noswagger"], description = "Disables SwaggerUI at endpoint '/swagger'", converter = BooleanConverter::class)
+    @Parameter(names = ["-noswagger", "--noswagger"], description = "Disables SwaggerUI at endpoint '/swagger'", converter = BooleanConverter::class)
     var noSwaggerUi: Boolean = false
 
     @Parameter(names = ["-h", "--help"], help = true)

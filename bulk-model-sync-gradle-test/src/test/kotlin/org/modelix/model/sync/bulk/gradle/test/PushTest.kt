3ab93a075102b574abe375329cfcf4c0a51e51dc
Michael Huster <mhuster@itemis.com>
2024-02-20 11:13:56 +0100

    test(bulk-model-sync-gradle): simplify PushTest

--- bulk-model-sync-gradle-test/src/test/kotlin/org/modelix/model/sync/bulk/gradle/test/PushTest.kt
+++ bulk-model-sync-gradle-test/src/test/kotlin/org/modelix/model/sync/bulk/gradle/test/PushTest.kt
@@ -8,16 +8,17 @@ import org.junit.jupiter.api.TestInstance
 import org.modelix.model.ModelFacade
 import org.modelix.model.api.BuiltinLanguages
 import org.modelix.model.api.ConceptReference
-import org.modelix.model.api.IBranch
+import org.modelix.model.api.INode
 import org.modelix.model.api.IProperty
+import org.modelix.model.api.PBranch
 import org.modelix.model.api.getDescendants
 import org.modelix.model.api.getRootNode
 import org.modelix.model.client2.ModelClientV2PlatformSpecificBuilder
-import org.modelix.model.client2.getReplicatedModel
 import org.modelix.model.data.ModelData
 import org.modelix.model.data.NodeData
 import org.modelix.model.lazy.RepositoryId
 import org.modelix.model.sync.bulk.asExported
+import org.modelix.model.withAutoTransactions
 import java.io.File
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
@@ -28,55 +29,47 @@ class PushTest {
     private val branchRef = ModelFacade.createBranchReference(RepositoryId("ci-test"), "master")
     private val client = ModelClientV2PlatformSpecificBuilder().url(url).build().apply { runBlocking { init() } }
 
-    private fun runTest(body: (IBranch) -> Unit) {
-        val replicatedModel = client.getReplicatedModel(branchRef)
-        val branch = runBlocking { replicatedModel.start() }
-        body(branch)
-        replicatedModel.dispose()
+    private fun runTest(body: (INode) -> Unit) {
+        val baseVersion = runBlocking { client.pullIfExists(branchRef)!! }
+        val branch = PBranch(baseVersion.getTree(), client.getIdGenerator()).withAutoTransactions()
+        body(branch.getRootNode())
     }
 
     @Test
-    fun `nodes were synced to server`() = runTest { branch ->
+    fun `nodes were synced to server`() = runTest { root ->
         val inputDir = File("build/model-sync/testPush")
         val files = inputDir.listFiles()?.filter { it.extension == "json" } ?: error("no json files found in ${inputDir.absolutePath}")
 
         val modules = files.map { ModelData.fromJson(it.readText()) }
         val inputModel = ModelData(root = NodeData(children = modules.map { it.root }))
 
-        branch.runRead {
-            assertContentEquals(inputModel.root.children, branch.getRootNode().allChildren.map { it.asExported() })
-        }
+        assertContentEquals(inputModel.root.children, root.allChildren.map { it.asExported() })
     }
 
     @Test
-    fun `meta properties were applied to root node`() = runTest { branch ->
-        branch.runRead {
-            val actual1 = branch.getRootNode().getPropertyValue(IProperty.fromName("metaKey1"))
-            val actual2 = branch.getRootNode().getPropertyValue(IProperty.fromName("metaKey2"))
+    fun `meta properties were applied to root node`() = runTest { root ->
+        val actual1 = root.getPropertyValue(IProperty.fromName("metaKey1"))
+        val actual2 = root.getPropertyValue(IProperty.fromName("metaKey2"))
 
-            assertEquals("metaValue1", actual1)
-            assertEquals("metaValue2", actual2)
-        }
+        assertEquals("metaValue1", actual1)
+        assertEquals("metaValue2", actual2)
     }
 
     @Test
-    fun `cross module references were synced`() = runTest { branch ->
-        branch.runRead {
-            val rootNode = branch.getRootNode()
-            val solution1Graph = checkNotNull(
-                rootNode.allChildren
-                    .find { it.getPropertyValue(BuiltinLanguages.jetbrains_mps_lang_core.INamedConcept.name) == "GraphSolution" }
-                    ?.getDescendants(false)
-                    ?.find { it.getConceptReference() == ConceptReference(_C_UntypedImpl_Graph.getUID()) },
-            )
+    fun `cross module references were synced`() = runTest { root ->
+        val solution1Graph = checkNotNull(
+            root.allChildren
+                .find { it.getPropertyValue(BuiltinLanguages.jetbrains_mps_lang_core.INamedConcept.name) == "GraphSolution" }
+                ?.getDescendants(false)
+                ?.find { it.getConceptReference() == ConceptReference(_C_UntypedImpl_Graph.getUID()) },
+        )
 
-            val solution2Graph = checkNotNull(
-                rootNode.allChildren
-                    .find { it.getPropertyValue(BuiltinLanguages.jetbrains_mps_lang_core.INamedConcept.name) == "GraphSolution2" }
-                    ?.getDescendants(false)
-                    ?.find { it.getConceptReference() == ConceptReference(_C_UntypedImpl_Graph.getUID()) },
-            )
-            assertEquals(solution1Graph, solution2Graph.getReferenceTarget(C_Graph.relatedGraph))
-        }
+        val solution2Graph = checkNotNull(
+            root.allChildren
+                .find { it.getPropertyValue(BuiltinLanguages.jetbrains_mps_lang_core.INamedConcept.name) == "GraphSolution2" }
+                ?.getDescendants(false)
+                ?.find { it.getConceptReference() == ConceptReference(_C_UntypedImpl_Graph.getUID()) },
+        )
+        assertEquals(solution1Graph, solution2Graph.getReferenceTarget(C_Graph.relatedGraph))
     }
 }

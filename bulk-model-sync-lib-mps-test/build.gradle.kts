dfb5a42672ccfd7b88b5cdb53a25cda97eb03634
Oleksandr Dzhychko <odzhychko@itemis.com>
2024-06-03 12:15:07 +0200

    build(bulk-model-sync): make tests for bulk-model-sync-lib with MPS a subproject
    Previously they were a separate Gradle project.

--- bulk-model-sync-lib-mps-test/build.gradle.kts
+++ bulk-model-sync-lib-mps-test/build.gradle.kts
@@ -14,8 +14,10 @@
  * limitations under the License.
  */
 
+import org.modelix.copyMps
+
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    kotlin("jvm")
     // We are not building an actual plugin here.
     // We use/abuse the gradle-intellij-plugin run tests with MPS.
     // (With enough time and effort,
@@ -23,72 +25,24 @@ plugins {
     // and build something custom using the relevant parts.
     // For the time being, this solution works without much overhead and great benefit.)
     alias(libs.plugins.intellij)
-}
-
-val modelixCoreVersion: String = file("../version.txt").readText()
-
-version = modelixCoreVersion
-
-repositories {
-    val modelixRegex = "org\\.modelix.*"
-    mavenLocal {
-        content {
-            includeGroupByRegex(modelixRegex)
-        }
-    }
-    gradlePluginPortal {
-        content {
-            excludeGroupByRegex(modelixRegex)
-        }
-    }
-    maven {
-        url = uri("https://artifacts.itemis.cloud/repository/maven-mps/")
-        content {
-            includeGroupByRegex(modelixRegex)
-            includeGroup("com.jetbrains")
-        }
-    }
-    mavenCentral {
-        content {
-            excludeGroupByRegex(modelixRegex)
-        }
-    }
+    id("modelix-project-repositories")
 }
 
 dependencies {
-    testImplementation("org.modelix:bulk-model-sync-lib:$modelixCoreVersion")
-    testImplementation("org.modelix.mps:model-adapters:$modelixCoreVersion")
+    testImplementation(project(":bulk-model-sync-lib"))
+    testImplementation(project(":mps-model-adapters"))
     testImplementation(libs.kotlin.serialization.json)
 }
 
-val mpsVersion = project.findProperty("mps.version")?.toString()?.takeIf { it.isNotEmpty() }
-    ?: "2021.1.4"
-println("Building for MPS version $mpsVersion")
-
-// Extract MPS during configuration phase, because using it in intellij.localPath requires it to already exist.
-val mpsHome = project.layout.buildDirectory.dir("mps-$mpsVersion")
-val mpsZip: Configuration by configurations.creating
-dependencies { mpsZip("com.jetbrains:mps:$mpsVersion") }
-mpsHome.get().asFile.let { baseDir ->
-    if (baseDir.exists()) return@let // the content of MPS zip is not expected to change
-
-    println("Extracting MPS ...")
-    sync {
-        from(zipTree({ mpsZip.singleFile }))
-        into(mpsHome)
-    }
-
-    // The build number of a local IDE is expected to contain a product code, otherwise an exception is thrown.
-    val buildTxt = mpsHome.get().asFile.resolve("build.txt")
-    val buildNumber = buildTxt.readText()
-    val prefix = "MPS-"
-    if (!buildNumber.startsWith(prefix)) {
-        buildTxt.writeText("$prefix$buildNumber")
-    }
-
-    println("Extracting MPS done.")
+intellij {
+    localPath = copyMps().absolutePath
 }
 
-intellij {
-    localPath = mpsHome.map { it.asFile.absolutePath }
+tasks {
+    // Workaround:
+    // * Execution failed for task ':bulk-model-sync-lib-mps-test:buildSearchableOptions'.
+    // * > Cannot find IDE platform prefix. Please create a bug report at https://github.com/jetbrains/gradle-intellij-plugin. As a workaround specify `idea.platform.prefix` system property for task `buildSearchableOptions` manually.
+    buildSearchableOptions {
+        enabled = false
+    }
 }

4ac2181963bb6040252c3f74dca36a577e95e2f7
Oleksandr Dzhychko <odzhychko@itemis.com>
2024-06-06 15:06:06 +0200

    fix(model-server): fix `IgniteStoreClient.getAll` not returning values for existing entries
    With different field order in Ignite configuration and key type `ObjectInRepository`, `IgniteCache.getAll` returned null values because keys were serialized differently.
    This only happened when `IgniteCache.getAll` was called with more than one key and the keys were not loaded into the cache.
    The serialization of a key return from a read-through operation was different from the serialization of a key created from an `ObjectInRepository` object.

--- model-server/src/main/kotlin/org/modelix/model/server/store/ObjectInRepository.kt
+++ model-server/src/main/kotlin/org/modelix/model/server/store/ObjectInRepository.kt
@@ -16,6 +16,14 @@
 
 package org.modelix.model.server.store
 
+/**
+ * A composed key used in the key-value stores to persist model data.
+ * The key is composed of a [key] and an optional [repositoryId].
+ * The [repositoryId] is set when an entry is scoped to the repository.
+ *
+ * This class is directly used with the Ignite cache (see [IgniteStoreClient]).
+ * Therefore, the order of fields must be consistent with the configuration of `JdbcType` in ignite.xml.
+ */
 data class ObjectInRepository(private val repositoryId: String, val key: String) : Comparable<ObjectInRepository> {
     fun isGlobal() = repositoryId == ""
     fun getRepositoryId() = repositoryId.takeIf { it != "" }

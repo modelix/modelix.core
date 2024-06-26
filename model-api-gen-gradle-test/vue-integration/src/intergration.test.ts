97b84952d05412e04ca69adad265e965d7d95a17
Oleksandr Dzhychko <odzhychko@itemis.com>
2024-06-26 08:49:35 +0200

    WIP add explanation about identity of INodeJS and ITypedNode

--- model-api-gen-gradle-test/vue-integration/src/intergration.test.ts
+++ model-api-gen-gradle-test/vue-integration/src/intergration.test.ts
@@ -12,7 +12,7 @@ import {
   Classifier,
 } from "typescript-generation/dist/L_jetbrains_mps_baseLanguage";
 
-import { ITypedNode, LanguageRegistry } from "@modelix/ts-model-api";
+import { INodeJS, ITypedNode, LanguageRegistry } from "@modelix/ts-model-api";
 
 registerLanguages();
 
@@ -135,3 +135,103 @@ test("change to reference is reactivly updated", () => {
 
   expect(computedClassifierName.value).toBe("aName");
 });
+
+// Implementation of the wrapper cache.
+function getWraperCache() {
+  const cache = new WeakMap<INodeJS, ITypedNode>();
+  return function getWrapperFromCache(newWrapper: ITypedNode): ITypedNode {
+    const untypedNode = newWrapper.unwrap();
+    const oldWrapper = cache.get(untypedNode);
+    if (oldWrapper !== undefined) {
+      return oldWrapper;
+    } else {
+      cache.set(untypedNode, newWrapper);
+      return newWrapper;
+    }
+  };
+}
+
+test("typed node objects can be cached", () => {
+  try {
+    const nodeData = {
+      root: {
+        children: [
+          {
+            // concecpt ID of an INamedConcept
+            concept: "mps:ceab5195-25ea-4f22-9b92-103b95ca8c0c/1169194658468",
+            role: "children1",
+            properties: {
+              name: "aName",
+            },
+          },
+        ],
+      },
+    };
+    // Using the wrapper cache.
+    LanguageRegistry.INSTANCE.wrapperCache = getWraperCache();
+    const rootNode = useRootNode(nodeData);
+    const untypedNodeCreatedFirst = rootNode.getChildren("children1")[0];
+    const untypedNodeCreatedSecond = rootNode.getChildren("children1")[0];
+    const typedNodeCreatedFirst = LanguageRegistry.INSTANCE.wrapNode(
+      untypedNodeCreatedFirst,
+    );
+    const typedNodeCreatedSecond = LanguageRegistry.INSTANCE.wrapNode(
+      untypedNodeCreatedSecond,
+    );
+
+    // Make comparison inside `expect` function,
+    // because differences between properties do not help you to understand why a test fails.
+    // > If differences between properties do not help you to understand why a test fails,
+    // > especially if the report is large,
+    // > then you might move the comparison into the expect function.
+    // See https://jestjs.io/docs/29.6/expect#tobevalue
+    expect(typedNodeCreatedFirst === typedNodeCreatedSecond).toBe(true);
+  } finally {
+    LanguageRegistry.INSTANCE.wrapperCache = undefined;
+  }
+});
+
+test("untyped node objects can be cached", () => {
+  const nodeData = {
+    root: {
+      children: [
+        {
+          // concecpt ID of an INamedConcept
+          concept: "mps:ceab5195-25ea-4f22-9b92-103b95ca8c0c/1169194658468",
+          role: "children1",
+          properties: {
+            name: "aName",
+          },
+        },
+      ],
+    },
+  };
+  const rootNode = useRootNode(nodeData);
+  const untypedNodeCreatedFirst = rootNode.getChildren("children1")[0];
+  const untypedNodeCreatedSecond = rootNode.getChildren("children1")[0];
+
+  // Works without workaround when INodeJS is `ReactiveINodeJS`,
+  // because `ReactiveINodeJS` are always create with `toReactiveINodeJS` whicht utilizes a cache.
+  // NOTE: `===` would not work, if we used plain `INodeJS` returned by `@modelix/model-client`.
+  expect(untypedNodeCreatedFirst === untypedNodeCreatedSecond).toBe(true);
+  expect(untypedNodeCreatedFirst.equals(untypedNodeCreatedSecond)).toBe(true);
+});
+
+/*
+  Background in JVM and Kotlin objects have `.equals`/`.hashCode`.
+  (Note, that when you write in `==` in Kotlin it is converted to an `.equals` call in JS.)
+
+  Because in JVM and Kotlin we have `.equals`/`.hashCode` we can create many `INode` objects for one "conceptual" node.
+
+  To always return the same UNTYPED object for the same "conceptual" node but different `INodeJS` objects of it,
+  we would need to introduce caching, on the levels of `INodeJS`-create.
+  This is actually already done for `ReactiveINodeJS` but actually for reasons specific to Vue.js and not equality.
+
+  To always return the same TYPED object, we need to additionally set a `
+  LanguageRegistry.INSTANCE.wrapperCache = getWraperCache()`.
+  Check out the custom `getWraperCache` in this test.
+
+  Still my suggestion is:
+    * Avoid caches. Do not bother wiht `getWraperCache`. Do not use `===`/`==` in TS. Always compare the references.
+    * For ease of use, use a util method `isSameNode` or add it as `.same`/`.equals`/`.isSameNode` method to `ITypedNode`.
+*/

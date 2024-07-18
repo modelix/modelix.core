modelix/modelix.core

commit 86c141bdb73b5623c7954ead0de03d1156ed4d6c

fix(model-api-gen): fix IConcept.conceptAlias() always returning null for generated concepts
Generated concepts did not implement getConceptProperty(name: String).

model-api-gen/src/main/kotlin/org/modelix/metamodel/generator/internal/ConceptObjectGenerator.kt
@@ -67,6 +67,7 @@ internal class ConceptObjectGenerator(
             addSuperclassConstructorParameter(concept.abstract.toString())
             addInstanceClassGetter()
             addTypedFun()
+            addConceptPropertiesGetter()
             addLanguageProperty()
             addWrapFun()
             if (concept.uid != null) {
@@ -97,6 +98,25 @@ internal class ConceptObjectGenerator(
             )
         }
         addProperty(propertySpec)
+    }
+
+    private fun TypeSpec.Builder.addConceptPropertiesGetter() {
+        if (concept.metaProperties.isEmpty()) return
+
+        val getConceptPropertyFun = FunSpec.builder(GeneratedConcept<*, *>::getConceptProperty.name).runBuild {
+            val paramName = GeneratedConcept<*, *>::getConceptProperty.parameters.first().name ?: "name"
+            returns(String::class.asTypeName().copy(nullable = true))
+            addParameter(paramName, String::class)
+            addModifiers(KModifier.OVERRIDE)
+            beginControlFlow("return when (%N)", paramName)
+            for ((key, _) in concept.metaProperties) {
+                addStatement("""%S -> %T.%N""", key, concept.conceptWrapperInterfaceClass(), key)
+            }
+            addStatement("else -> null")
+            endControlFlow()
+        }
+
+        addFunction(getConceptPropertyFun)
     }
 
     private fun TypeSpec.Builder.addConceptObjectChildLink(childLink: ProcessedChildLink) {

model-api-gen-gradle-test/kotlin-generation/src/test/kotlin/org/modelix/metamodel/generator/GeneratedApiTest.kt
@@ -16,6 +16,7 @@ import org.modelix.metamodel.typed
 import org.modelix.metamodel.untyped
 import org.modelix.model.ModelFacade
 import org.modelix.model.api.INode
+import org.modelix.model.api.conceptAlias
 import org.modelix.model.api.getRootNode
 import org.modelix.model.data.ConceptData
 import org.modelix.model.data.ModelData
@@ -82,6 +83,11 @@ class GeneratedApiTest {
     fun `metaProperty alias has value`() {
         val alias = C_ClassConcept.alias
         assertEquals("class", alias)
+    }
+
+    @Test
+    fun `alias can be retrieved via conceptAlias`() {
+        assertEquals(C_ClassConcept.alias, C_ClassConcept.untyped().conceptAlias())
     }
 
     private fun KAnnotatedElement.hasDeprecationWithMessage() =

model-api-gen/src/main/kotlin/org/modelix/metamodel/generator/internal/ConceptWrapperInterfaceGenerator.kt
@@ -89,8 +89,7 @@ internal class ConceptWrapperInterfaceGenerator(
     private fun TypeSpec.Builder.addConceptMetaPropertiesIfNecessary() {
         if (conceptPropertiesInterfaceName == null) return
 
-        // TODO use IConcept.getConceptProperty(name: String)
-        concept.metaProperties.forEach { (key, value) ->
+        for ((key, value) in concept.metaProperties) {
             val propertySpec = PropertySpec.builder(key, String::class.asTypeName()).runBuild {
                 addModifiers(KModifier.OVERRIDE)
                 initializer("%S", value)

commit 8696d04c4ea6574aecc25e006c670c4f24ccf150

fix(model-api-gen): fix generated code not compiling if conceptPropertiesInterfaceName was not set
This was a regression introduced in commit 86c141bd.

model-api-gen/src/main/kotlin/org/modelix/metamodel/generator/internal/ConceptFileGenerator.kt
@@ -43,7 +43,7 @@ internal class ConceptFileGenerator(
 ) : NameConfigBasedGenerator(nameConfig), FileGenerator {
 
     override fun generateFileSpec(): FileSpec {
-        val conceptObject = ConceptObjectGenerator(concept, nameConfig).generate()
+        val conceptObject = ConceptObjectGenerator(concept, nameConfig, conceptPropertiesInterfaceName).generate()
         val conceptWrapperInterface = ConceptWrapperInterfaceGenerator(
             concept,
             nameConfig,

model-api-gen/src/main/kotlin/org/modelix/metamodel/generator/internal/ConceptObjectGenerator.kt
@@ -53,6 +53,7 @@ import kotlin.reflect.KClass
 internal class ConceptObjectGenerator(
     private val concept: ProcessedConcept,
     override val nameConfig: NameConfig,
+    private val conceptPropertiesInterfaceName: String?,
     private val alwaysUseNonNullableProperties: Boolean = true,
 ) : NameConfigBasedGenerator(nameConfig) {
 
@@ -101,7 +102,7 @@ internal class ConceptObjectGenerator(
     }
 
     private fun TypeSpec.Builder.addConceptPropertiesGetter() {
-        if (concept.metaProperties.isEmpty()) return
+        if (conceptPropertiesInterfaceName == null || concept.metaProperties.isEmpty()) return
 
         val getConceptPropertyFun = FunSpec.builder(GeneratedConcept<*, *>::getConceptProperty.name).runBuild {
             val paramName = GeneratedConcept<*, *>::getConceptProperty.parameters.first().name ?: "name"
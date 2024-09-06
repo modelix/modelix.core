50a7dec90b963a41320cd6dbd702fd69b6364e14
Oleksandr Dzhychko <odzhychko@itemis.com>
2024-09-06 16:25:33 +0200

    build(deps): remove org.jetbrains.kotlinx-kotlinx-collections-immutable

--- model-datastructure/build.gradle.kts
+++ model-datastructure/build.gradle.kts
@@ -11,7 +11,6 @@ kotlin {
                 api(project(":model-api"))
 //                api(project(":model-server-api"))
 //                kotlin("stdlib-common")
-//                implementation(libs.kotlin.collections.immutable)
 //                implementation(libs.kotlin.coroutines.core)
                 implementation(libs.kotlin.logging)
                 implementation(libs.kotlin.datetime)

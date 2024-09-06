2bb2470c966ea308f95de371c9591c6105b9745a
Oleksandr Dzhychko <odzhychko@itemis.com>
2024-09-06 16:27:27 +0200

    build(model-datastructure): clean up build.gradle.kts

--- model-datastructure/build.gradle.kts
+++ model-datastructure/build.gradle.kts
@@ -9,15 +9,8 @@ kotlin {
             dependencies {
                 api(project(":kotlin-utils"))
                 api(project(":model-api"))
-//                api(project(":model-server-api"))
-//                kotlin("stdlib-common")
-//                implementation(libs.kotlin.coroutines.core)
                 implementation(libs.kotlin.logging)
                 implementation(libs.kotlin.datetime)
-//                implementation(libs.kotlin.serialization.json)
-//                implementation(libs.ktor.client.core)
-//                implementation(libs.ktor.client.content.negotiation)
-//                implementation(libs.ktor.serialization.json)
             }
         }
         val commonTest by getting {
@@ -27,42 +20,14 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
-//                kotlin("stdlib-jdk8")
-//
-//                implementation(libs.vavr)
-//                implementation(libs.apache.commons.lang)
-//                implementation(libs.guava)
-//                implementation(libs.apache.commons.io)
-//                implementation("org.json:json:20230618")
                 implementation(libs.trove4j)
                 implementation(libs.apache.commons.collections)
                 implementation(libs.kotlin.coroutines.core)
-//
-//                implementation("com.google.oauth-client:google-oauth-client:1.34.1")
-//                implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
-//
-//                implementation(libs.ktor.client.core)
-//                implementation(libs.ktor.client.cio)
-//                implementation(libs.ktor.client.auth)
-//                implementation(libs.ktor.client.content.negotiation)
-//                implementation(libs.ktor.serialization.json)
-            }
-        }
-        val jvmTest by getting {
-            dependencies {
             }
         }
         val jsMain by getting {
             dependencies {
-//                implementation(kotlin("stdlib-js"))
-//                implementation(npm("uuid", "^8.3.0"))
-//                implementation(npm("js-sha256", "^0.9.0"))
                 implementation(npm("@aws-crypto/sha256-js", "^5.0.0"))
-//                implementation(npm("js-base64", "^3.4.5"))
-            }
-        }
-        val jsTest by getting {
-            dependencies {
             }
         }
     }

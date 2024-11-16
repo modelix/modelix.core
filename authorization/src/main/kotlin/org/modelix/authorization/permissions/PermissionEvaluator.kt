dae4cdb9efa845f2b5c424f252daab21a0082b53
slisson <sl@q60.de>
2024-11-16 08:14:35 +0100

    fix(authorization): ignore unknown granted permissions
    fix MODELIX-1018

--- authorization/src/main/kotlin/org/modelix/authorization/permissions/PermissionEvaluator.kt
+++ authorization/src/main/kotlin/org/modelix/authorization/permissions/PermissionEvaluator.kt
@@ -1,5 +1,7 @@
 package org.modelix.authorization.permissions
 
+import org.modelix.authorization.UnknownPermissionException
+
 class PermissionEvaluator(val schemaInstance: SchemaInstance) {
     private val allGrantedPermissions: MutableSet<PermissionInstanceReference> = HashSet()
     private val parser = PermissionParser(schemaInstance.schema)
@@ -7,11 +9,15 @@ class PermissionEvaluator(val schemaInstance: SchemaInstance) {
     fun getAllGrantedPermissions(): Set<PermissionInstanceReference> = schemaInstance.getAllPermissions().map { it.ref }.filter { hasPermission(it) }.toSet()
 
     fun grantPermission(permissionId: String) {
-        grantPermission(parser.parse(permissionId))
+        grantPermission(PermissionParts.fromString(permissionId))
     }
 
     fun grantPermission(permissionId: PermissionParts) {
-        grantPermission(parser.parse(permissionId))
+        try {
+            grantPermission(parser.parse(permissionId))
+        } catch (ex: UnknownPermissionException) {
+            // Tokens may also contain permissions for other services.
+        }
     }
 
     fun grantPermission(permissionRef: PermissionInstanceReference) {

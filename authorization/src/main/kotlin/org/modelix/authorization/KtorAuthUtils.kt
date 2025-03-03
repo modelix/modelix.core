0d85f9f9644358a6784c8693ac2aa5424858dc90
Sascha Lisson <sl@q60.de>
2025-03-03 18:00:41 +0100

    fix(authorization): inconsistent usage of DecodedJWT and Payload
    Some methods still expected a `DecodedJWT` where a `Payload` is sufficient.
    
    BREAKING CHANGE: AccessTokenPrincipal was replaced by Ktors JWTPrincipal

--- authorization/src/main/kotlin/org/modelix/authorization/KtorAuthUtils.kt
+++ authorization/src/main/kotlin/org/modelix/authorization/KtorAuthUtils.kt
@@ -7,10 +7,10 @@ import io.ktor.http.auth.AuthScheme
 import io.ktor.http.auth.HttpAuthHeader
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationCall
-import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.application.plugin
 import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.parseAuthorizationHeader
 import io.ktor.server.auth.principal
 import io.ktor.server.request.header
@@ -41,7 +41,7 @@ fun RoutingContext.checkPermission(permissionParts: PermissionParts) {
 
 fun ApplicationCall.checkPermission(permissionToCheck: PermissionParts) {
     if (!hasPermission(permissionToCheck)) {
-        val principal = principal<AccessTokenPrincipal>()
+        val principal = principal<JWTPrincipal>()
         throw NoPermissionException(principal, null, null, "${principal?.getUserName()} has no permission '$permissionToCheck'")
     }
 }
@@ -76,19 +76,17 @@ fun ApplicationCall.getBearerToken(): String? {
     return tokenString
 }
 
-fun ApplicationCall.jwtFromHeaders(): DecodedJWT? {
+fun ApplicationCall.getUnverifiedJwt(): DecodedJWT? {
     // OAuth proxy passes the ID token as the bearer token, but we need the access token.
     return (request.header("X-Forwarded-Access-Token") ?: getBearerToken())?.let { JWT.decode(it) }
 }
-
-fun ApplicationCall.jwt() = principal<AccessTokenPrincipal>()?.jwt ?: jwtFromHeaders()
 
 fun RoutingContext.getUserName(): String? {
     return call.getUserName()
 }
 
 fun ApplicationCall.getUserName(): String? {
-    return principal<AccessTokenPrincipal>()?.getUserName()
+    return principal<JWTPrincipal>()?.getUserName()
 }
 
 @Deprecated("Use ModelixAuthorizationConfig.nullIfInvalid")
--- authorization/src/main/kotlin/org/modelix/authorization/NoPermissionException.kt
+++ authorization/src/main/kotlin/org/modelix/authorization/NoPermissionException.kt
@@ -1,10 +1,12 @@
 package org.modelix.authorization
 
-class NoPermissionException(val user: AccessTokenPrincipal?, val resourceId: String?, val scope: String?, message: String) :
+import io.ktor.server.auth.jwt.JWTPrincipal
+
+class NoPermissionException(val user: JWTPrincipal?, val resourceId: String?, val scope: String?, message: String) :
     RuntimeException(message) {
 
     constructor(message: String) :
         this(null, null, null, message)
-    constructor(user: AccessTokenPrincipal, permissionId: String, type: String) :
+    constructor(user: JWTPrincipal, permissionId: String, type: String) :
         this(user, permissionId, type, "${user.getUserName()} has no $type permission on '$permissionId'")
 }

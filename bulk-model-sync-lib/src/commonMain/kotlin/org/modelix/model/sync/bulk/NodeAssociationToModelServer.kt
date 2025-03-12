6481e2362582c0855ac7690e80c1e85d5088a09c
Sascha Lisson <sl@q60.de>
2025-03-12 12:51:31 +0100

    perf(mps-sync-plugin): implementation of `NodeAssociationToModelServer` was slow

--- bulk-model-sync-lib/src/commonMain/kotlin/org/modelix/model/sync/bulk/NodeAssociationToModelServer.kt
+++ bulk-model-sync-lib/src/commonMain/kotlin/org/modelix/model/sync/bulk/NodeAssociationToModelServer.kt
@@ -1,31 +1,38 @@
 package org.modelix.model.sync.bulk
 
-import org.modelix.model.ModelIndex
 import org.modelix.model.api.IBranch
 import org.modelix.model.api.IMutableModel
 import org.modelix.model.api.IReadableNode
 import org.modelix.model.api.IWritableNode
 import org.modelix.model.api.NodeReference
 import org.modelix.model.api.PNodeAdapter
+import org.modelix.model.api.getDescendants
 import org.modelix.model.api.getOriginalOrCurrentReference
 import org.modelix.model.api.getOriginalReference
+import org.modelix.model.api.getRootNode
 import org.modelix.model.area.PArea
 import org.modelix.model.data.NodeData
 
 class NodeAssociationToModelServer(val branch: IBranch) : INodeAssociation {
 
-    private val modelIndex
-        get() = ModelIndex.get(branch.transaction, NodeData.Companion.ID_PROPERTY_KEY)
+    private val associations: MutableMap<String, IWritableNode> by lazy {
+        val map = HashMap<String, IWritableNode>()
+        for (node in branch.getRootNode().getDescendants(true).map { it.asWritableNode() }) {
+            map[node.getPropertyValue(NodeData.ID_PROPERTY_REF) ?: continue] = node
+        }
+        map
+    }
 
     override fun resolveTarget(sourceNode: IReadableNode): IWritableNode? {
         val ref = sourceNode.getOriginalOrCurrentReference()
-        return modelIndex.find(ref).map { PNodeAdapter(it, branch) }.firstOrNull()?.asWritableNode()
+        return associations[ref]
             ?: PArea(branch).resolveNode(NodeReference(ref))?.asWritableNode()
     }
 
     override fun associate(sourceNode: IReadableNode, targetNode: IWritableNode) {
         val expected = sourceNode.getOriginalOrCurrentReference()
         if (expected != targetNode.getOriginalOrCurrentReference()) {
+            associations[expected] = targetNode
             targetNode.setPropertyValue(NodeData.ID_PROPERTY_REF, expected)
         }
     }

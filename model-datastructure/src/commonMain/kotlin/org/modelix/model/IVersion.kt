7b0493556ce470f90f304a7ea0f130e012b1fd75
Sascha Lisson <sl@q60.de>
2025-03-22 10:17:02 +0100

    feat(model-datastructure): a version can now contain multiple trees
    Having a separate tree for the metamodel instead of putting model and metamodel into the same tree
    makes the diffing faster. You can more quickly detect that the metamodel didn't change by just
    comparing the hash of the tree. Some listener that processes metamodel changes then doesn't have to
    process and ignore all the unrelated model changes.

--- model-datastructure/src/commonMain/kotlin/org/modelix/model/IVersion.kt
+++ model-datastructure/src/commonMain/kotlin/org/modelix/model/IVersion.kt
@@ -1,8 +1,28 @@
 package org.modelix.model
 
 import org.modelix.model.api.ITree
+import kotlin.jvm.JvmInline
 
 interface IVersion {
     fun getContentHash(): String
     fun getTree(): ITree
+    fun getTrees(): Map<TreeType, ITree>
+}
+
+@JvmInline
+value class TreeType(val name: String) {
+    init {
+        for (c in name) {
+            if (!('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || '0' <= c && c <= '9')) {
+                throw IllegalArgumentException("Name contains illegal characters: $name")
+            }
+        }
+    }
+
+    override fun toString(): String = name.ifEmpty { "<main>" }
+
+    companion object {
+        val MAIN = TreeType("")
+        val META = TreeType("meta")
+    }
 }

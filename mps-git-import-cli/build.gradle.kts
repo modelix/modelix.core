26ac534330ca385ceed1fef7dea1387cb7e3b837
Sascha Lisson <sl@q60.de>
2025-04-20 21:33:07 +0200

    ci: publish images with skaffold

--- mps-git-import-cli/build.gradle.kts
+++ mps-git-import-cli/build.gradle.kts
@@ -1,26 +1,14 @@
-import org.modelix.mpsHomeDir
 
 plugins {
-    `modelix-kotlin-jvm-with-junit`
+    kotlin("jvm")
     alias(libs.plugins.jib)
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 dependencies {
     implementation(project(":mps-git-import"))
     implementation(libs.modelix.buildtools.lib)
-
-    compileOnly(
-        fileTree(mpsHomeDir).matching {
-            include("lib/**/*.jar")
-        },
-    )
-}
-
-jib {
-    from {
-        image = "modelix/mps-git-import-base"
-    }
-    to {
-        image = "modelix/mps-git-import"
-    }
 }

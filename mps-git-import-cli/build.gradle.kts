8c166a3865702093908eb21bfe63812e72539066
Sascha Lisson <sl@q60.de>
2025-04-21 07:38:58 +0200

    build: manual publishing of the mps-base-image
    The base image usually only changes when there is a new MPS version.
    Publishing it with every modelix.core is unnecessary.

--- mps-git-import-cli/build.gradle.kts
+++ mps-git-import-cli/build.gradle.kts
@@ -12,3 +12,7 @@ dependencies {
     implementation(project(":mps-git-import"))
     implementation(libs.modelix.buildtools.lib)
 }
+
+jib {
+    from.image = "modelix/mps-base-image:2024.3@sha256:fdaebe708aabe17add687143a585b06a7eb412b3529eff413bc37b2c13dfac8e"
+}

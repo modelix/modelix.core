d94820c1a0255676ac9a9103772e3e3ddf0d2809
Sascha Lisson <sl@q60.de>
2025-05-26 14:51:49 +0200

    test: use testcontainers in model-client-js-test for starting a model-server instance

--- model-client-js-test/ci.sh
+++ model-client-js-test/ci.sh
@@ -4,17 +4,8 @@ set -e
 (
   TEST_DIR="$(dirname "$(readlink -f "$0")")"
   cd "$TEST_DIR/../"
-  if [ "${CI}" != "true" ]; then
-    trap cleanup INT TERM EXIT
-    cleanup () {
-      kill "${MODEL_SERVER_PID}"
-      exit
-    }
-  fi
-  ./gradlew :model-server:run --console=plain --args="-inmemory -port 28103" &
-  MODEL_SERVER_PID=$!
+  ./gradlew :model-server:jibDockerBuild
 
-  curl -X GET --retry 30 --retry-connrefused --retry-delay 1 http://localhost:28103/health
   cd "$TEST_DIR"
   ./gradlew build --console=plain --stacktrace
 )
